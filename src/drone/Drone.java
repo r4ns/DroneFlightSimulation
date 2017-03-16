14874a70f8c6810f1593eb30cb0f510438a9dac2
itc14 <itc14@ws8.itc03.edu>
2017-03-16 20:53:38 +0100

    Filip Lapcevic It26-2015

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,52 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone {
+	
+	int x = 30;
+	int y = 0;
+	int z = 30;
+
+	@Override
+	public String moveUp() {
+		this.y = this.y+1;
+		return "Nove koordinate su: x - "+x+", y - "+y+", z - "+z;
+	}
+
+	@Override
+	public String moveDown() {
+		this.y = this.y-1;
+		return "Nove koordinate su: x - "+x+", y - "+y+", z - "+z;
+	}
+
+	@Override
+	public String moveLeft() {
+		this.x = this.x-1;
+		return "Nove koordinate su: x - "+x+", y - "+y+", z - "+z;
+	}
+
+	@Override
+	public String moveRight() {
+		this.x = this.x+1;
+		return "Nove koordinate su: x - "+x+", y - "+y+", z - "+z;
+
+	}
+
+	@Override
+	public String moveBack() {
+		this.z = this.z-1;
+		return "Nove koordinate su: x - "+x+", y - "+y+", z - "+z;
+	}
+
+	@Override
+	public String moveForth() {
+		this.z = this.z+1;
+		return "Nove koordinate su: x - "+x+", y - "+y+", z - "+z;
+	}
+
+	@Override
+	public String getFormatedCoordinates() {
+		// TODO Auto-generated method stub
+		return "Nove koordinate su: x - "+x+", y - "+y+", z - "+z;
+	}
 
 }

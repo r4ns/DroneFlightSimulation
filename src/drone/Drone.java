467262cdf0889daf4c9cc774237133311b8b9125
it15g2015 <it15g2015@ws8.itc03.edu>
2017-03-17 20:41:54 +0100

    Krajnji test

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,61 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone{
+	
+	int x;
+	int y;
+	int z;
+	
+	Drone(int x,int y,int z){
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	}
+	
+	public String moveUp(){
+		x=this.x;
+		y=this.y+1;
+		z=this.z;
+		System.out.println("Drone position(x:"+this.x+" y:"+this.y+" z:"+this.z+")");
+		return "Drone position(x:"+x+" y:"+y+" z:"+z+")";
+	}
+	public String moveDown(){
+		x=this.x;
+		y=this.y-1;
+		z=this.z;
+		System.out.println("Drone position(x:"+this.x+" y:"+this.y+" z:"+this.z+")");
+		return "Drone position(x:"+x+" y:"+y+" z:"+z+")";
+	}
+	public String moveLeft(){
+		x=this.x-1;
+		y=this.y;
+		z=this.z;
+		System.out.println("Drone position(x:"+this.x+" y:"+this.y+" z:"+this.z+")");
+		return "Drone position(x:"+x+" y:"+y+" z:"+z+")";
+	}
+	public String moveRight(){
+		x=this.x+1;
+		y=this.y;
+		z=this.z;
+		System.out.println("Drone position(x:"+this.x+" y:"+this.y+" z:"+this.z+")");
+		return "Drone position(x:"+x+" y:"+y+" z:"+z+")";
+	}
+	public String moveBack(){
+		x=this.x;
+		y=this.y;
+		z=this.z+1;
+		System.out.println("Drone position(x:"+this.x+" y:"+this.y+" z:"+this.z+")");
+		return "Drone position(x:"+x+" y:"+y+" z:"+z+")";
+	}
+	public String moveForth(){
+		x=this.x;
+		y=this.y;
+		z=this.z-1;
+		System.out.println("Drone position(x:"+this.x+" y:"+this.y+" z:"+this.z+")");
+		return "Drone position(x:"+x+" y:"+y+" z:"+z+")";
+	}
+	public String getFormatedCoordinates(){
+		return "Drone position(x:"+this.x+" y:"+this.y+" z:"+this.z+")";
+	}
 
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -1,9 +1,66 @@
 package drone;
 
-public class DroneController {
+public class DroneController{
 
 	public static void main(String[] args) {		
-
-
+		Drone drone = new Drone(30,0,30);
+		System.out.println("---------------moving UP------------------");
+		for(int i=0; i<6; i++)
+		{
+			drone.moveUp();
+		}
+		System.out.println("---------------moving LEFT------------------");
+		for(int i=0; i<25; i++)
+		{
+			drone.moveLeft();
+		}
+		System.out.println("---------------moving UP------------------");
+		for(int i=0; i<6; i++)
+		{
+			drone.moveUp();
+		}
+		System.out.println("---------------moving BACK------------------");
+		for(int i=0; i<15; i++)
+		{
+			drone.moveBack();
+		}
+		System.out.println("---------------moving RIGHT------------------");
+		for(int i=0; i<16; i++)
+		{
+			drone.moveRight();
+		}
+		System.out.println("---------------moving UP------------------");
+		for(int i=0; i<30; i++)
+		{
+			drone.moveUp();
+		}
+		System.out.println("---------------moving FORTH------------------");
+		for(int i=0; i<42; i++)
+		{
+			drone.moveForth();
+		}
+		System.out.println("---------------moving DOWN------------------");
+		for(int i=0; i<12; i++)
+		{
+			drone.moveDown();
+		}
+		System.out.println("---------------moving LEFT------------------");
+		for(int i=0; i<20; i++)
+		{
+			drone.moveLeft();
+		}
+		System.out.println("---------------moving BACK------------------");
+		for(int i=0; i<24; i++)
+		{
+			drone.moveBack();
+		}
+		System.out.println("---------------moving LEFT------------------");
+		drone.moveLeft();
+		
+		System.out.println("---------------moving BACK------------------");
+		drone.moveBack();
+		drone.moveBack();
+		drone.moveBack();
+		
 	}
 }

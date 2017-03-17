1f5304395a8acd01ce93ef3e71e494628ef06291
aleksandarbabic <alekski64@icloud.com>
2017-03-17 15:33:21 +0100

    Napredak, moveUp, moveDown

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,63 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone{
+	
+	private int currentX;
+	private int currentY;
+	private int currentZ;
+	
+	public Drone() {
+		currentX = 30;
+		currentY = 0;
+		currentZ = 30;
+		System.out.println(getFormatedCoordinates());
+	}
+	
+	@Override
+	public String moveUp() {
+		if(currentY<10 && (currentX>=0 && currentX<=30))
+			currentY++;
+		if(currentY<50 && (currentX >=40 && currentX<=50))
+			currentY++;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveDown() {
+		if(currentY>0)
+			currentY--;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveLeft() {
+		if(currentX > 0)
+			currentX--;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveRight() {
+		if(currentX>=30 && currentX<50)
+			currentX++;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveBack() {
+		// TODO Auto-generated method stub
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveForth() {
+		// TODO Auto-generated method stub
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String getFormatedCoordinates() {
+		return "Drone position : (" +currentX+","+ currentY+","+currentZ + ")";
+	}
 
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -3,7 +3,13 @@ package drone;
 public class DroneController {
 
 	public static void main(String[] args) {		
-
+		Drone mojDron = new Drone();
+		for(int i = 0;i<10;i++)
+		System.out.println(mojDron.moveUp());
+		for(int i = 0;i<45;i++)
+			System.out.println(mojDron.moveRight());
+		for(int i = 0;i<45;i++)
+			System.out.println(mojDron.moveUp());
 
 	}
 }

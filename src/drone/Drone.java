50ed516227087be52ee6cf078956b28edada0c94
Sara Havzi <havzisara@gmail.com>
2017-03-18 00:04:41 +0100

    Hej :)

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,93 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone{
+	private int x, y, z;
+	//KONSTRUKTOR
+	public Drone(){
+		
+	}
+	public Drone(int x, int y, int z){
+		this.x=x;
+		this.y=y;
+		this.z=z;	
+	}
+	
+	//METODE IZ INTERFEJSA
+	public String moveUp(){
+		this.y=y+1;
+		return this.getFormatedCoordinates();
+	};
+	public String moveDown(){
+		this.y=y-1;
+		return this.getFormatedCoordinates();
+	};
+	public String moveLeft(){
+		this.x=x-1;
+		return this.getFormatedCoordinates();
+	};
+	public String moveRight(){
+		this.x=x+1;
+		return this.getFormatedCoordinates();
+	};
+	public String moveBack(){
+		this.z=z-1;
+		return this.getFormatedCoordinates();
+	};
+	public String moveForth(){
+		this.z=z+1;
+		return this.getFormatedCoordinates();
+	};
+	public String getFormatedCoordinates(){
+		return "Drone position: ("+x+","+y+","+z+")";
+	}
+	//SIDES
+	public String sides(){
+		//DRON JE NAPRED
+		if(x>=0 && x<50&& y>=0 && y<=50 && z>=0 && z<=10){
+			if(x>=40 && x<50)
+			{
+				return "Drone is on the right side"; //DRON JE DESNO
+			}
+			else return "Drone is on the front side";
+		}
+		//DRON JE NAZAD
+		if(x>=0 && x<=50 && y>=0 && y<=50 && z>=40 && z<=50){
+			return "Drone is on the back side";
+		}
+		//DRON JE LEVO
+		else if ( x>=0 && x<=10 && y>=0 && y<=50 && z>=0 && z<=50)
+		{
+			return "Drone is on the left side";
+		}
+		//DRON JE DOLE
+		else if (x>=0 && x<=50 && y>=0 && y<=10 && z>=0 && z<=50){
+			return "Drone is on the bottom side";
+		}
+		//DRON JE GORE
+		else if (x>=0 && x<=50 && y>=40 && y<=50 && z>=0 && z<=50){
+			return "Drone is on the up side";
+		}
+		else return "";
+	}
+	//GETTERI I SETTERI
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getZ() {
+		return z;
+	}
+	public void setZ(int z) {
+		this.z = z;
+	};
+	
 
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -3,7 +3,76 @@ package drone;
 public class DroneController {
 
 	public static void main(String[] args) {		
-
+		//DRON OBAVESTAVA NA KOJOJ JE STRANI SAMO KADA JE PROMENI
+		Drone drone = new Drone(30,0,30);
+		System.out.println(drone.getFormatedCoordinates()); //Pocetne koordinate
+		System.out.println("------------------------------");
+		System.out.println("Drone is flying");
+		System.out.println("------------------------------");
+		drone.moveUp(); //Dron polece
+		System.out.println(drone.getFormatedCoordinates()); 
+		
+		System.out.println(drone.sides()); //Trebalo bi da izbaci dole
+		
+		System.out.println("------------------------------");
+		System.out.println("Drone is moving back");
+		System.out.println("------------------------------");
+		for (int i=0; i<25; i++){
+			drone.moveBack();
+			System.out.println(drone.getFormatedCoordinates());
+		}
+		System.out.println("------------------------------");
+		System.out.println(drone.sides()); //Trebalo bi da izbaci napred
+		System.out.println("------------------------------");
+		
+		System.out.println("------------------------------");
+		System.out.println("Drone is moving right");
+		System.out.println("------------------------------");
+		for (int i=0; i<15; i++){
+			drone.moveRight();
+			System.out.println(drone.getFormatedCoordinates());
+		}
+		System.out.println("------------------------------");
+		System.out.println(drone.sides()); //Trebalo bi da izbaci desno
+		System.out.println("------------------------------");
+		
+		System.out.println("------------------------------");
+		System.out.println("Drone is moving up");
+		System.out.println("------------------------------");
+		for (int i=0; i<29; i++){
+			drone.moveUp();
+			System.out.println(drone.getFormatedCoordinates());
+		}
+		
+		System.out.println("------------------------------");
+		System.out.println("Drone is moving forward");
+		System.out.println("------------------------------");
+		for(int i=0; i<40; i++){
+			drone.moveForth();
+			System.out.println(drone.getFormatedCoordinates());
+		}
+		System.out.println("------------------------------");
+		System.out.println(drone.sides()); // Trebalo bi da izbaci nazad
+		System.out.println("------------------------------");
+		
+		System.out.println("------------------------------");
+		System.out.println("Drone is moving left");
+		System.out.println("------------------------------");
+		for(int i=0; i<45; i++){
+			drone.moveLeft();
+			System.out.println(drone.getFormatedCoordinates());
+		}
+		System.out.println("------------------------------");
+		System.out.println("Drone is moving back");
+		System.out.println("------------------------------");
+		for(int i=0; i<15; i++){
+			drone.moveBack();
+			System.out.println(drone.getFormatedCoordinates());
+		}
+		System.out.println("------------------------------");
+		System.out.println(drone.sides()); // Trebalo bi da izbaci levo
+		System.out.println("Target reached");
+		System.out.println("------------------------------");
 
 	}
 }

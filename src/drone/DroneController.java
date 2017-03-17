2f54427330e5dcf75046aeefa1cadde3ae1f6c82
itc3 <itc3@ws1.itc03.edu>
2017-03-17 20:48:56 +0100

    it8

--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -3,7 +3,11 @@ package drone;
 public class DroneController {
 
 	public static void main(String[] args) {		
-
+     Drone dr = new Drone (30,0,30);
+     dr.moveUp();
+     dr.moveRight();
+     dr.moveUp();
+     dr.moveForth();
 
 	}
 }

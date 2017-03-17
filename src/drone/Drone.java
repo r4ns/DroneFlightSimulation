1d743f32d188e787cf7fbbe8eff997f0010339f4
IT58-2015 <IT58-2015@AR1-04.mshome.net>
2017-03-17 15:42:43 +0100

    Aleksandra Prpic IT58/2015

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,66 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone {
+	private int x;
+	private int y;
+	private int z;
+	
+	private String yStr;
+	private String xStr;
+	
+	public Drone(int x, int y, int z){
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	}
+	
+	public String toString() {
+		return "Drone position: (" + this.x +", " + this.y + ", " +this.z + ")";
+	}
+	@Override
+	public String moveUp() {
+		y=y+1;
+		yStr=String.valueOf(y);
+		return yStr;
+	}
+
+	@Override
+	public String moveDown() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String moveLeft() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String moveRight() {
+		x =x+1;
+		//xStr=String.valueOf(x);
+		return null;
+	}
+
+	@Override
+	public String moveBack() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String moveForth() {
+		z=z-1;
+		return null;
+	}
+
+	@Override
+	public String getFormatedCoordinates() {
+		
+		return null;
+	}
+	
+	
 
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -3,7 +3,28 @@ package drone;
 public class DroneController {
 
 	public static void main(String[] args) {		
-
-
+		Drone dron = new Drone(30, 0, 30);
+		System.out.println("Pocetna pozicija!");
+		System.out.println(dron);
+		
+		System.out.println("*****Pomeram se gore!*****");
+		
+		for (int i = 0; i <10; i++){
+			dron.moveUp();
+			System.out.println(dron);
+		}
+		System.out.println("*****Pomeram se desno!*****");
+		for (int i = 0; i <10; i++){
+		dron.moveRight();
+		System.out.println(dron);
+		}
+		
+		System.out.println("*****Pomeram se gore!*****");
+		for (int i = 0; i <30; i++){
+			dron.moveUp();
+			System.out.println(dron);
+		}
+		dron.moveForth();
+		System.out.println(dron);
 	}
 }

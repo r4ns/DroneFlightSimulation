b5ffc5835bec2bd9c277b49fbad27783a9e3353b
Milos Covilo <miloscovilo@hotmail.com>
2017-03-17 21:11:36 +0100

    V1.0

--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -11,81 +11,81 @@ public class DroneController {
 			Drone gore = new Drone(pocetnoX,pocetnoy,pocetnoZ);
 			
 			System.out.println("Polecemo!!!");
+			System.out.println("--------GORE--------");
 			int i = 0;
 			while(i<5){
 				System.out.println(gore.moveUp());
 				i++;
 			}
-			System.out.println("Popeli smo se po y za 5 !!!");
-			
+			System.out.println("--------NAZAD--------");
 			int j = 0;
 			while(j<15){
 				System.out.println(gore.moveBack());
 				j++;
 			}
-			System.out.println("Vratili smo se nazad po z za 15 !!!");
+
+			System.out.println("--------GORE--------");
 			
 			int z = 0;
 			while(z<40){
 				System.out.println(gore.moveUp());
 				z++;
 			}
-			System.out.println("Penjemo se  smo po y za 40 !!!");
-			
+			System.out.println("--------NAPRED--------");
 			int y = 0;
 			while(y<40){
 				System.out.println(gore.moveForth());
 				y++;
 			}
-			System.out.println("Idemo napred po z za 40 !!!");
-			
+
+			System.out.println("--------DOLE--------");
 			int k = 0;
 			while(k<40){
 				System.out.println(gore.moveDown());
 				k++;
 			}
-			System.out.println("Idemo dole po y za 40 !!!");
-			
+
+			System.out.println("--------DESNO--------");
 			int m = 0;
 			while(m<15){
 				System.out.println(gore.moveRight());
 				m++;
 			}
-			System.out.println("Idemo desno po x za 15 !!!");
+			System.out.println("--------NAZAD--------");
 			
 			int n = 0;
 			while(n<25){
 				System.out.println(gore.moveBack());
 				n++;
 			}
-			System.out.println("Vracamo se nazad po z za 30 !!!");
-			
+
+			System.out.println("--------GORE--------");
 			int c = 0;
 			while(c<40){
 				System.out.println(gore.moveUp());
 				c++;
 			}
-			System.out.println("Idemo gore po y za 40");
+			System.out.println("--------LEVO--------");
 			
 			int v = 0;
 			while(v<40){
 				System.out.println(gore.moveLeft());
 				v++;
 			}
-			System.out.println("Idemo levo po x");
+			System.out.println("--------DOLE--------");
 			
 			int g = 0;
 			while(g<15){
 				System.out.println(gore.moveDown());
 				g++;
 			}
-			System.out.println("Idemo dole po y za 15");
+			System.out.println("--------LEVO--------");
 			
 			int p = 0;
 			while(p<5){
 				System.out.println(gore.moveLeft());
 				p++;
 			}
-			System.out.println("Idemo levo po x za 5");
+
 	}
 }

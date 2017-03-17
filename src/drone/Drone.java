654caffb0636022848ddb685e4bb3423ed28f849
it1g2015 <it1g2015@ws9.itc03.edu>
2017-03-17 20:38:07 +0100

    Aleksandar Koprivica v1

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,58 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone{
+	
+	int x=30;
+	int y=0;
+	int z=30;
+	
+	
+	public Drone(int x, int y, int z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	
+	@Override
+	public String moveUp() {
+		return ("Drone position ("+x+","+y+","+z+")");
+	}
 
+	@Override
+	public String moveDown() {
+		// TODO Auto-generated method stub
+		return ("Drone position ("+x+","+y+","+z+")");
+	}
+
+	@Override
+	public String moveLeft() {
+		// TODO Auto-generated method stub
+		return ("Drone position ("+x+","+y+","+z+")");
+	}
+
+	@Override
+	public String moveRight() {
+		// TODO Auto-generated method stub
+		return ("Drone position ("+x+","+y+","+z+")");
+	}
+
+	@Override
+	public String moveBack() {
+		// TODO Auto-generated method stub
+		return ("Drone position ("+x+","+y+","+z+")");
+	}
+
+	@Override
+	public String moveForth() {
+		// TODO Auto-generated method stub
+		return ("Drone position ("+x+","+y+","+z+")");
+	}
+
+	@Override
+	public String getFormatedCoordinates() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -2,8 +2,39 @@ package drone;
 
 public class DroneController {
 
-	public static void main(String[] args) {		
-
-
+	public static void main(String[] args) {	
+		Drone MojDron = new Drone(30,0,30);
+		while (MojDron.y < 10) {
+			System.out.println(MojDron.moveUp());
+			MojDron.y++;
+		}
+		while (MojDron.x < 50) {
+			System.out.println(MojDron.moveUp());
+			MojDron.x++;
+		}
+		while (MojDron.y < 50) {
+			System.out.println(MojDron.moveUp());
+			MojDron.y++;
+		}
+		while (MojDron.x > 30) {
+			System.out.println(MojDron.moveUp());
+			MojDron.x--;
+		}
+		while (MojDron.z > 0) {
+			System.out.println(MojDron.moveUp());
+			MojDron.z--;
+		}
+		while (MojDron.y > 30) {
+			System.out.println(MojDron.moveUp());
+			MojDron.y--;
+		}
+		while (MojDron.x > 0) {
+			System.out.println(MojDron.moveUp());
+			MojDron.x--;
+		}
+		while (MojDron.z <= 30) {
+			System.out.println(MojDron.moveUp());
+			MojDron.z++;
+		}
 	}
 }

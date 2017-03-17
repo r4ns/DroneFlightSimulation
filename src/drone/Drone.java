624cce6c54e7db25d796c3a0afb3d3fe44e5ca16
it42g2015 <it42g2015@ws7.itc03.edu>
2017-03-17 13:05:12 +0100

    Igor Sember IT42-2015

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,90 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone {
+	private int x = 30;
+	private int y = 0;
+	private int z = 30;
+	
+	
+	
+	public Drone(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;				
+	}
+	
+	public String moveUp() {
+		if(y < 10){
+			y += 1;
+		}
+		return this.toString();
+	}
+	public String moveDown() {
+		if(y < 0){
+			y -= 1;
+		}
+		return this.toString();
+	}
+	public String moveLeft() {
+		if(x > 40){
+			x -= 1;
+		}
+		return this.toString();
+	}
+	public String moveRight() {
+		if(x < 50){
+		x += 1;
+		}
+		return this.toString();
+	}
+	public String moveBack() {
+		if(z < 50){
+		z += 1;
+		}
+		return this.toString();
+	}
+	public String moveForth() {
+		if(z > 0){
+		z -= 1;
+		}
+		return this.toString();
+	}
+	
+	
+	public String toString() {
+		return String.format("Pozicija ("+x+","+y+","+z+")");
+	}
+	
+	public String getFormatedCoordinates() {
+		return this.toString();
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
+	}
+	
+	
+	
+	
 
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -1,8 +1,11 @@
 package drone;
 
-public class DroneController {
+public class DroneController  {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+		Drone dr = new Drone(30,0,30);
+		dr.moveUp();
+		
 
 
 	}

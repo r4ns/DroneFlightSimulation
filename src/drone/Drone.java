a8702cd55dfec620fd3064166e15ef7ca3d7dab9
IT17g2015 <IT17g2015@WS3.itc03.edu>
2017-03-17 20:41:32 +0100

    CAO

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,79 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone{
+
+	private int xKordinata;
+	private int yKordinata;
+	private int zKordinata;
+	
+	private int brojUdarenihIvica;
+	
+
+	public Drone(int xKordinata, int yKordinata, int zKordinata){
+		this.xKordinata = xKordinata;
+		this.yKordinata = yKordinata;
+		this.zKordinata = zKordinata;
+	}
+	
+	@Override
+	public String moveUp() {
+		yKordinata++;
+	
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveDown() {
+		yKordinata--;
+		
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveLeft() {
+		xKordinata--;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveRight() {
+		xKordinata++;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveBack() {
+		zKordinata--;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveForth() {
+		zKordinata++;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String getFormatedCoordinates() {
+		/*
+		if(xKordinata == 0 || xKordinata == 50 || yKordinata == 0 || yKordinata == 50 || zKordinata == 0 || zKordinata == 50)
+			System.out.print("Drone hits outer square border: ");
+		if(xKordinata == 10 || xKordinata == 40 || yKordinata == 10 || yKordinata == 40 || zKordinata == 10 || zKordinata == 50)
+			System.out.print("Drone hits inner square border: ");
+			*/
+		return "Drone position (" + xKordinata +  ", " + yKordinata + ", " + zKordinata + ")";
+	}
+	
+	public int getxKordinata() {
+		return xKordinata;
+	}
+
+	public int getyKordinata() {
+		return yKordinata;
+	}
+
+	public int getzKordinata() {
+		return zKordinata;
+	}
 
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -4,6 +4,24 @@ public class DroneController {
 
 	public static void main(String[] args) {		
 
-
+		
+		Drone d = new Drone(30, 0, 30);
+		
+		
+		while(d.getzKordinata() < 50)
+			System.out.println(d.moveForth());
+		while(d.getyKordinata() < 30)
+			System.out.println(d.moveUp());
+		while(d.getxKordinata() < 50)
+			System.out.println(d.moveRight());
+		while(d.getzKordinata() > 0)
+			System.out.println(d.moveBack());
+		
+		while(d.getxKordinata() > 0)
+			System.out.println(d.moveLeft());
+		
+		while(d.getzKordinata() < 30)
+			System.out.println(d.moveForth());
+		
 	}
 }

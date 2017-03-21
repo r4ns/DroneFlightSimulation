3913e08acc24a4a48370202e53af0ff1862e9c0e
repac <repac@aleks>
2017-03-21 21:08:29 +0100

    Aleksandra Repac IT31-2015

--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -4,6 +4,47 @@ public class DroneController {
 
 	public static void main(String[] args) {		
 
-
+		Drone drone = new Drone();
+		int i;
+		
+		for(i=drone.y; i<10;i++)
+		{
+			drone.moveUp();
+			
+		}
+		for(i=drone.x; i<=49; i++)
+		{
+			drone.moveRight();
+		}
+		
+		for(i=drone.y; i<50; i++)
+		{
+			drone.moveUp();
+		}
+		
+		for (i=drone.z; i>10; i--)
+		{
+			drone.moveForth();
+		}
+		
+		for (i=drone.x; i>10; i--)
+		{
+			drone.moveLeft();
+		}
+		
+		for (i=drone.z; i<30; i++)
+		{
+			drone.moveBack();
+		}
+		
+		for (i=drone.y; i>30; i--)
+		{
+			drone.moveDown();
+		}
+		
+		for (i=drone.x; i>0; i--)
+		{
+			drone.moveLeft();
+		}
 	}
 }

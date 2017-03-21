c0ad425e57e9605c1466e9c2be14b87d7cf6f4a3
9910 <webdevmastersensei@gmail.com>
2017-03-21 20:09:52 +0100

    Fancy output

--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -6,21 +6,27 @@ public class DroneController {
 
 		Drone nekiDrone = new Drone();
 		
+		System.out.println("---------------Move Up--------------");
 		for(int i = 0; i < 50; i++)
 			System.out.println(nekiDrone.moveUp());
 		
+		System.out.println("---------------Move Left--------------");
 		for(int i = 0; i < 50; i++)
 			System.out.println(nekiDrone.moveLeft());
 		
+		System.out.println("---------------Move Up--------------");
 		for(int i = 0; i < 50; i++)
 			System.out.println(nekiDrone.moveUp());
 		
+		System.out.println("---------------Move Right--------------");
 		for(int i = 0; i < 50; i++)
 			System.out.println(nekiDrone.moveRight());
-
+		
+		System.out.println("---------------Move Down--------------");
 		for(int i = 0; i < 50; i++)
 			System.out.println(nekiDrone.moveDown());
 
+		System.out.println("---------------Move Forth--------------");
 		for(int i = 0; i < 50; i++)
 			System.out.println(nekiDrone.moveForth());
 		

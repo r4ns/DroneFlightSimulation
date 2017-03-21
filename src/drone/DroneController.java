7165b186f606b05d22ea5f732f465c2732f62f60
Danilo <Danilo@DESKTOP-NHUL435>
2017-03-21 19:24:50 +0100

    Zavrsna verzija

--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -17,37 +17,34 @@ public class DroneController {
 			System.out.println(dron.moveUp());
 		}
 		System.out.println("Gotovo");
-		for(int i=0;i<=16;i++){
-			System.out.println(dron.moveForth());
-		}
-		System.out.println("Gotovo");
-		for(int i=0;i<=10;i++){
-			System.out.println(dron.moveLeft());
-		}
-		System.out.println("Gotovo, sad pravo");
-		for(int i=0;i<=5;i++){
-			System.out.println(dron.moveForth());
-		}
-		System.out.println("Gotovo, sad levo");
-		for(int i=0;i<=18;i++){
-			System.out.println(dron.moveLeft());
-		}
-		System.out.println("Gotovo, sad gore");
-		for(int i=0;i<=17;i++){
-			System.out.println(dron.moveUp());
-		}
-		System.out.println("Gotovo nazad");
-		for(int i=0;i<=19;i++){
+		for(int i=0;i<=20;i++){
 			System.out.println(dron.moveBack());
 		}
 		System.out.println("Gotovo");
-		for(int i=0;i<=25;i++){
+		for(int i=0;i<=20;i++){
 			System.out.println(dron.moveLeft());
+		}
+		System.out.println("Gotovo");
+		for(int i=0;i<=23;i++){
+			System.out.println(dron.moveUp());
+		}
+		System.out.println("Gotovo");
+		for(int i=0;i<=25;i++){
+			System.out.println(dron.moveForth());
+		}
+		System.out.println("Gotovo");
+		for(int i=0;i<=28;i++){
+			System.out.println(dron.moveLeft());
+		}
+		System.out.println("Gotovo");
+		for(int i=0;i<=4;i++){
+			System.out.println(dron.moveBack());
 		}
 		System.out.println("Gotovo");
 		for(int i=0;i<=19;i++){
 			System.out.println(dron.moveDown());
 		}
+		
 		if(dron.getX()==0&&dron.getY()==30&&dron.getZ()==30)
 			System.out.println("Uspeo");
 		

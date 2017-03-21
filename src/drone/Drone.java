19819310cf0e231a065a06f9d17d90098e8888f0
Milunka <Milunka@DESKTOP-EUVP5DK>
2017-03-21 22:40:37 +0100

    Jovana Savic IT18-2015

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,97 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone {
+	
+	private int x = 30;
+	private int y = 0;
+	private int z = 30;
+	
+	
+	public Drone(){
+		
+	}
+	public Drone(int x, int y, int z){
+		
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	
+
+	@Override
+	public String moveUp() {
+		
+		y++;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveDown() {
+		y--;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveLeft() {
+		x--;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveRight() {
+		x++;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveBack() {
+		z--;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveForth() {
+		z++;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String getFormatedCoordinates() {
+		
+		System.out.println(("(" + this.x + "," + this.y + "," + this.z + ")"));
+		
+		return ("(" + this.x + "," + this.y + "," + this.z + ")") ;
+	}
+
+
+	public int getX() {
+		return x;
+	}
+
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+
+	public int getY() {
+		return y;
+	}
+
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+
+	public int getZ() {
+		return z;
+	}
+
+
+	public void setZ(int z) {
+		this.z = z;
+	}
 
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -2,7 +2,58 @@ package drone;
 
 public class DroneController {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {	
+		
+		Drone dron = new Drone(30,0,30);
+		
+		for (int i = 1; i<5; i++)
+		{
+			dron.moveUp();
+		}
+		System.out.println();
+		
+		for (int i = 1; i<13; i++)
+		{
+			dron.moveRight();
+		}
+		System.out.println();
+		
+		for (int i = 1; i<42; i++)
+		{
+			dron.moveUp();
+		}
+		System.out.println();
+		
+		for(int i = 1; i<18; i++)
+		{
+			dron.moveLeft();
+		}
+		System.out.println();
+		for(int i = 1; i<13;i++)
+		{
+			dron.moveForth();
+		}
+		System.out.println();
+		for(int i = 1;i<16; i++)
+		{
+			dron.moveDown();
+		}
+		System.out.println();
+		for(int i =1; i <23; i++)
+		{
+			dron.moveLeft();
+		}
+		System.out.println();
+		for (int i = 1; i<13; i++)
+		{
+			dron.moveBack();
+		}
+		System.out.println();
+		for(int i = 1; i<4;i++)
+		{
+			dron.moveLeft();
+		}
+		
 
 
 	}

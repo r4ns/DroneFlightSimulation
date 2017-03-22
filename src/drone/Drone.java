785afd5a80462841e95e59d37361dc99a3067049
Ana <Ana@Ana-PC>
2017-03-22 01:38:06 +0100

    IT36-2015 Ana GLigorić

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,82 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone {
+	 private int x;
+	 private int y;
+	 private int z;
+	public Drone(){
+		
+	}
+	public Drone(int x, int y, int z){
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	}
+	
+	public String moveUp() {
+		setY(y+1);
+		getFormatedCoordinates();
+		return x + ","+ y + "," + z;
+		
+	}
+
+	@Override
+	public String moveDown() {
+		setY(y-1);
+		getFormatedCoordinates();
+		return x + ","+ y + "," + z;
+	}
+
+	@Override
+	public String moveLeft() {
+		setX(x - 1);
+		getFormatedCoordinates();
+		return x + ","+ y + "," + z;
+	}
+
+	@Override
+	public String moveRight() {
+		setX(x + 1);
+		getFormatedCoordinates();
+		return x + ","+ y + "," + z;
+	}
+
+	@Override
+	public String moveBack() {
+		setZ(z+1);
+		getFormatedCoordinates();
+		return x + ","+ y + "," + z;
+	}
+
+	@Override
+	public String moveForth() {
+		setZ(z-1);
+		getFormatedCoordinates();
+		return x + ","+ y + "," + z;
+	}
+
+	@Override
+	public String getFormatedCoordinates() {
+		System.out.println(x + ","+ y + "," + z);
+		return x + ","+ y + "," + z;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getZ() {
+		return z;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public void setZ(int z) {
+		this.z = z;
+	}
 
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -3,7 +3,31 @@ package drone;
 public class DroneController {
 
 	public static void main(String[] args) {		
-
-
+		Drone drone = new Drone(30,0,30);
+		drone.getFormatedCoordinates();
+		while(drone.getY()<10){
+			drone.moveUp();
+		}
+		while(drone.getX()<45){
+			drone.moveRight();		
+		}
+		while(drone.getZ()>5){
+			drone.moveForth();
+		}
+		while(drone.getY()<50){
+			drone.moveUp();
+		}
+		while(drone.getX()>30){
+			drone.moveLeft();
+		}
+		while(drone.getZ()<30){
+			drone.moveBack();
+		}
+		while(drone.getX()>0){
+			drone.moveLeft();
+		}
+		while(drone.getY()>30){
+			drone.moveDown();
+		}
 	}
 }

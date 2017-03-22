c0fa30817910c5fcb40a5e8140dc44b6c655f97e
Jelena <Jelena@DESKTOP-I3QAGR9>
2017-03-22 23:33:15 +0100

    Drone Flight Simulation

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,91 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone {
+    //levo desno po x
+	//gore dole po y
+	//napred oduzimamo od z nazad dodajemo vrednosti na z
+	
+	public int x;
+	public int y;
+	public int z;
+	
+	public Drone(){
+		
+	}
+	
+	public Drone(int x, int y,int z){
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	}
+	
+	@Override
+	public String moveUp() {
+		setY(y+1);
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveDown() {
+		setY(y-1);
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveLeft() {
+		setX(x-1);
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveRight() {
+		setX(x+1);
+		
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveBack() {
+		setZ(z+1);
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveForth() {
+		setZ(z-1);
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String getFormatedCoordinates() {
+		//System.out.println(x+ ", "+y+", "+z);
+		return "("+x + ", "+ y +", "+z+")";
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
+	}
+	
+	
 
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -2,7 +2,66 @@ package drone;
 
 public class DroneController {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {	
+		
+		Drone dron=new Drone(30,0,30);
+		System.out.println(dron.getFormatedCoordinates());
+		
+		System.out.println("-------Moving up-------");
+	    while(dron.getY()<10){
+	    	
+		 	dron.moveUp();
+		 	System.out.println(dron.getFormatedCoordinates());
+		 }
+	    
+	    System.out.println("-------Moving right-------");
+		 while(dron.getX()<45){
+			 
+		 	dron.moveRight();
+		 	System.out.println(dron.getFormatedCoordinates());
+		 }
+		 
+		 System.out.println("-------Moving forth-------");
+		 while(dron.getZ()>5){
+			 
+		 	dron.moveForth();
+		 	System.out.println(dron.getFormatedCoordinates());
+		 }
+		 
+		 System.out.println("-------Moving up-------");
+		 while(dron.getY()<50){
+			 
+		 	dron.moveUp();
+		 	System.out.println(dron.getFormatedCoordinates());
+		 }
+		 
+		 System.out.println("-------Moving left-------");
+		 while(dron.getX()>30){
+			 
+		 	dron.moveLeft();
+		 	System.out.println(dron.getFormatedCoordinates());
+		 }
+		 
+		 System.out.println("-------Moving back-------");
+		 while(dron.getZ()<30){
+			 
+		 	dron.moveBack();
+		 	System.out.println(dron.getFormatedCoordinates());
+		 }
+		 
+		 System.out.println("-------Moving left-------");
+		 while(dron.getX()>0){
+			 
+		 	dron.moveLeft();
+		 	System.out.println(dron.getFormatedCoordinates());
+		 }
+		 
+		 System.out.println("-------Moving down-------");
+		 while(dron.getY()>30){
+			 
+		 	dron.moveDown();
+		 	System.out.println(dron.getFormatedCoordinates());
+		 }
 
 
 	}

ad3073926552e02f6b5fcc0e4261523769ef3ea4
Comtrade <Comtrade@Comtrade-PC>
2017-03-22 08:38:49 +0100

    Milica Pavlovic IT-38/2015

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,56 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone{
+	int x=30, y=0, z=30;
+	@Override
+	public String moveUp() {
+		// TODO Auto-generated method stub
+		y++;
+		return getFormatedCoordinates();
+	}
+
+@Override
+	public String moveDown() {
+		// TODO Auto-generated method stub
+	y--;
+	return getFormatedCoordinates();
+	
+	}
+
+	@Override
+	public String moveLeft() {
+		// TODO Auto-generated method stub
+		
+		x--;
+		return getFormatedCoordinates();
+	}
+	
+	@Override
+	public String moveRight() {
+		x++;
+		return getFormatedCoordinates();
+	}
+		// TODO Auto-generated method stub
+		
+
+	@Override
+	public String moveBack() {
+		// TODO Auto-generated method stub
+	z++;
+	return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveForth() {
+		z--;
+		
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String getFormatedCoordinates() {
+		// TODO Auto-generated method stub
+		return "("+x+","+y+","+z+")";
+	}
 
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -3,7 +3,52 @@ package drone;
 public class DroneController {
 
 	public static void main(String[] args) {		
-
-
+		Drone drone=new Drone();
+		System.out.println("Podizanje gore!");
+		while(drone.y<10){
+		drone.moveUp();
+		System.out.println(drone.getFormatedCoordinates());
+		}
+		System.out.println("Pomeranje desno!");
+		while(drone.x<50){
+		drone.moveRight();
+		System.out.println(drone.getFormatedCoordinates());
+		}
+		System.out.println("Podizanje gore 2!");
+		while(drone.y<25){
+		drone.moveUp();
+		System.out.println(drone.getFormatedCoordinates());
+		}
+		System.out.println("Pomeranje nazad!");
+		while(drone.z<50){
+		drone.moveBack();
+		System.out.println(drone.getFormatedCoordinates());
+		}
+		System.out.println("Pomeranje levo!");
+		while(drone.x>25){
+		drone.moveLeft();
+		System.out.println(drone.getFormatedCoordinates());
+		}
+		System.out.println("Podizanje gore 3!");
+		while(drone.y<50){
+		drone.moveUp();
+		System.out.println(drone.getFormatedCoordinates());
+		}
+		System.out.println("Pomeranje napred!");
+		while(drone.z>30){
+		drone.moveForth();
+		System.out.println(drone.getFormatedCoordinates());
+		}
+		System.out.println("Pomeranje Levo!");
+		while(drone.x>0){
+			drone.moveLeft();
+			System.out.println(drone.getFormatedCoordinates());
+			}
+		System.out.println("Pomeranje dole !");
+		while(drone.y>30){
+		drone.moveDown();
+		System.out.println(drone.getFormatedCoordinates());
+		}
+		
 	}
 }

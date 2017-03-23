4e03be29c8c011a6407e033c7c3f50f152578ac9
pc <pc@pc-PC>
2017-03-23 15:59:37 +0100

    a

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -2,71 +2,216 @@ package drone;
 
 public class Drone implements StandardDrone{
 	
-	public int koordinataX;
-	public int koordinataY;
-	public int koordinataZ;
+	private int koordinataX;
+	private int koordinataY;
+	private int koordinataZ;
+	private boolean stop;
 	
+	public Drone(){
+		this.koordinataX = 30;
+		this.koordinataY = 0;
+		this.koordinataZ = 30;
+		this.stop = true;
+	}
+
 
 	public String getFormatedCoordinates() {
+		if(koordinataX==0 && koordinataY==30 && koordinataZ==30){
+			stop=false;
+			return "Drone position: (" + koordinataX + ", " + koordinataY + ", " + koordinataZ + ") //Target reached";
+		}
+		else
 		return "Drone position: (" + koordinataX + ", " + koordinataY + ", " + koordinataZ + ")";
-	 
 	}
 	
 	public String moveUp() {
 		if (koordinataY < 10 && koordinataX <= 40 && koordinataX >= 10 && koordinataZ <= 40 && koordinataZ >= 10){
 			koordinataY ++; 
+			if (koordinataY==10){
+				stop=false;
+				return getFormatedCoordinates() + "//Drone hits inner square border";
+			}
+			else{
+				return getFormatedCoordinates();
+			}
 		}
 		else if(koordinataY < 50){
 			koordinataY++;
+			if(koordinataY < 50){
+			return getFormatedCoordinates();}
+			else{
+				stop=false;
+				return getFormatedCoordinates() + "//Drone hits outer square border";
+			}
 		}
-		return getFormatedCoordinates(); 
+		else{
+			return getFormatedCoordinates();
+		}
 	}
 
 	public String moveDown() {
-		if (koordinataY < 50 && koordinataY >= 40 && koordinataX <=40 && koordinataX >= 10 && koordinataZ <= 40 && koordinataZ >= 10)
-		{koordinataY--;}
+		if (koordinataY > 40 && koordinataX <=40 && koordinataX >= 10 && koordinataZ <= 40 && koordinataZ >= 10){
+			koordinataY--;
+			if(koordinataY==40){
+				stop=false;
+				return getFormatedCoordinates() + "//Drone hits inner square border";
+			}
+			else{
+				return getFormatedCoordinates();
+			}
+		}
 		else if(koordinataY > 0)
 		{
 			koordinataY--;
+			if(koordinataY > 0){
+			return getFormatedCoordinates();}
+			else{
+				stop=false;
+				return getFormatedCoordinates() + "//Drone hits outer square border";
+			}
 		}
-		return getFormatedCoordinates();
+		else{
+			return getFormatedCoordinates();
+		}
 	}
 
-
 	public String moveLeft() {
-		if (koordinataY <= 40 && koordinataY >= 10 && koordinataX >=0 && koordinataZ <= 40 && koordinataZ >= 10)
-		{koordinataX++;}
-		else if(koordinataX <= 10)
-		{
-			koordinataX++;
+		if (koordinataY <= 40 && koordinataY >= 10 && koordinataX >40 && koordinataZ <= 40 && koordinataZ >= 10){
+			koordinataX--;
+			if(koordinataX==40){
+				stop=false;
+				return getFormatedCoordinates() + "//Drone hits inner square border";
+			}
+			else{
+				return getFormatedCoordinates();
+			}
 		}
-		return getFormatedCoordinates();
+		else if(koordinataX > 0)
+		{
+			koordinataX--;
+			if(koordinataX > 0){
+				return getFormatedCoordinates();}
+			else{
+				stop=false;
+				return getFormatedCoordinates() + "//Drone hits outer square border";
+				}
+		}
+		else{
+			return getFormatedCoordinates();
+		}
 	}
 
 	@Override
 	public String moveRight() {
-		if (koordinataY <= 40 && koordinataY >= 10 && koordinataX >40 && koordinataZ <= 40 && koordinataZ >= 10){
-			koordinataX ++; 
+		if ((koordinataY <= 40 && koordinataY >= 10) && koordinataX <10 && (koordinataZ <= 40 && koordinataZ >= 10)){
+			koordinataX ++;
+			if(koordinataX==10){
+				stop=false;
+				return getFormatedCoordinates() + "//Drone hits inner square border";
+			}
+			else{
+				return getFormatedCoordinates();
+			}
 		}
-		else if(koordinataY < 50){
+		else if(koordinataX < 50){
 			koordinataX++;
+			if (koordinataX==50){
+				stop=false;
+				return getFormatedCoordinates() + "//Drone hits outer square border";
+			}
+			else{
+				return getFormatedCoordinates();
+			}
 		}
-		return getFormatedCoordinates();
+		else{
+			return getFormatedCoordinates();
+		}
 	}
 
 	@Override
 	public String moveBack() {
-		// TODO Auto-generated method stub
-		return null;
+		if(koordinataX>=10 && koordinataX<=40 && koordinataY>=10 && koordinataY<=40 && koordinataZ<10){
+			koordinataZ++;
+			if(koordinataZ==10){
+				stop=false;
+				return getFormatedCoordinates() + "//Drone hits inner square border";
+			}
+			else{
+				return getFormatedCoordinates();
+			}
+		}
+		else if(koordinataZ<50){
+			koordinataZ++;
+			if (koordinataZ==50){
+				stop=false;
+				return getFormatedCoordinates() + "//Drone hits outer square border";
+			}
+			else{
+				return getFormatedCoordinates();
+			}
+			
+		}
+		else{
+			return getFormatedCoordinates();
+		}
 	}
 
 	@Override
 	public String moveForth() {
-		// TODO Auto-generated method stub
-		return null;
+		if(koordinataX>=10 && koordinataX<=40 && koordinataY>=10 && koordinataY<=40 && koordinataZ>40){
+			koordinataZ--;
+			if(koordinataZ==50){
+				stop=false;
+				return getFormatedCoordinates() + "//Drone hits inner square border";
+			}
+			else{
+				return getFormatedCoordinates();
+			}
+		}
+		else if(koordinataZ>0){
+			koordinataZ--;
+			if(koordinataZ > 0){
+				return getFormatedCoordinates();}
+				else{
+					stop=false;
+					return getFormatedCoordinates() + "//Drone hits outer square border";
+				}
+		}
+		else{
+			return getFormatedCoordinates();
+		}
 	}
 
-	
-	
+	public int getKoordinataX() {
+		return koordinataX;
+	}
+
+	public void setKoordinataX(int koordinataX) {
+		this.koordinataX = koordinataX;
+	}
+
+	public int getKoordinataY() {
+		return koordinataY;
+	}
+
+	public void setKoordinataY(int koordinataY) {
+		this.koordinataY = koordinataY;
+	}
+
+	public int getKoordinataZ() {
+		return koordinataZ;
+	}
+
+	public void setKoordinataZ(int koordinataZ) {
+		this.koordinataZ = koordinataZ;
+	}
+
+	public boolean isStop() {
+		return stop;
+	}
+
+	public void setStop(boolean stop) {
+		this.stop = stop;
+	}
 
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -2,8 +2,54 @@ package drone;
 
 public class DroneController {
 
-	public static void main(String[] args) {		
-
-
+	public static void main(String[] args) {
+		Drone dron = new Drone();
+		System.out.println(dron.getFormatedCoordinates());
+		System.out.println("-------------Moving up-------------");
+		while(dron.isStop()){
+			System.out.println(dron.moveUp());
+		}
+		dron.setStop(true);
+		System.out.println("-------------Moving back-------------");
+		while(dron.isStop()){
+			System.out.println(dron.moveBack());
+		}
+		dron.setStop(true);
+		System.out.println("-------------Moving up-------------");
+		while(dron.isStop()){
+			System.out.println(dron.moveUp());
+		}
+		System.out.println("-------------Moving forth-------------");
+		for(int i=0; i<20; i++){
+			System.out.println(dron.moveForth());
+		}
+		dron.setStop(true);
+		System.out.println("-------------Moving down-------------");
+		while(dron.isStop()){
+			System.out.println(dron.moveDown());
+		}
+		System.out.println("-------------Moving right-------------");
+		for(int i=0; i<3; i++){
+			System.out.println(dron.moveRight());
+		}
+		dron.setStop(true);
+		System.out.println("-------------Moving forth-------------");
+		while(dron.isStop()){
+			System.out.println(dron.moveForth());
+		}
+		dron.setStop(true);
+		System.out.println("-------------Moving left-------------");
+		while(dron.isStop()){
+			System.out.println(dron.moveLeft());
+		}
+		System.out.println("-------------Moving down-------------");
+		for(int i=0; i<10; i++){
+			System.out.println(dron.moveDown());
+		}
+		dron.setStop(true);
+		System.out.println("-------------Moving back-------------");
+		while(dron.isStop()){
+			System.out.println(dron.moveBack());
+		}
 	}
 }

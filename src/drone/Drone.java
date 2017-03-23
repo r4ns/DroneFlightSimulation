8e1264d487ee0a3b512c12ea3d8cf8f315adca96
Stefan Racic <Stefan Racic@DESKTOP-TQTE4EU>
2017-03-23 22:59:50 +0100

    Stefan Racic IT84-2016

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,59 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone {
+	
+	
+	public  int x;
+	public  int y;
+	public  int z;
+
+
+	Drone(int x,int y,int z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+
+	public  String moveUp() {
+		this.y++;
+		return "Drone position: ("+this.x+","+this.y+","+this.z+")";
+	}
+
+	@Override
+	public String moveDown() {
+		this.y--;
+		return "Drone position: ("+this.x+","+this.y+","+this.z+")";
+	}
+
+	@Override
+	public String moveLeft() {
+		this.x--;
+		return "Drone position: ("+this.x+","+this.y+","+this.z+")";
+	}
+
+	@Override
+	public String moveRight() {
+		this.x++;
+		return "Drone position: ("+this.x+","+this.y+","+this.z+")";
+	}
+
+	@Override
+	public String moveBack() {
+		this.z++;
+		return "Drone position: ("+this.x+","+this.y+","+this.z+")";
+	}
+
+	@Override
+	public String moveForth() {
+		this.z--;
+		return "Drone position: ("+this.x+","+this.y+","+this.z+")";
+	}
+
+	@Override
+	public String getFormatedCoordinates() {
+		return "Drone position: ("+this.x+","+this.y+","+this.z+")";
+	}
 
 }

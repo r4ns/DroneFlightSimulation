b7b31484a8938ad7eaa28adac60715fd356fdfa9
Nikola PC <Nikola PC@Nikola>
2017-03-23 14:12:27 +0100

    Nikola Zivkovic IT-41/2015

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,95 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone {
+	private int x;
+	private int y;
+	private int z;
+	
+	public Drone(){
+		
+	}
+	
+	public Drone(int x,int y,int z){
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	}
+	
+	
+	@Override
+	public String moveUp() {
+		this.y+=1;
+		return getFormatedCoordinates();
+	}
 
+	@Override
+	public String moveDown() {
+		this.y-=1;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveLeft() {
+		this.x-=1;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveRight() {
+		this.x+=1;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveBack() {
+		this.z+=1;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String moveForth() {
+		this.z-=1;
+		return getFormatedCoordinates();
+	}
+
+	@Override
+	public String getFormatedCoordinates() {
+		if(strigaoNaCilj()==true){
+			return "Dron je stigao na cilj!\nPozicija drona: (" + x + "," + y + "," + z + ")";
+		}
+		
+		return "Pozicija drona: (" + x + "," + y + "," + z + ")";
+	}
+	
+	public boolean strigaoNaCilj(){
+		if(this.x==0 && this.y==30 && this.z==30){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
+	}
 }

4bc43d8ca118bce6c93272fe504f8ec57737a56a
it36g2015 <it36g2015@WS3.itc03.edu>
2017-03-23 20:45:09 +0100

    novo

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -4,6 +4,9 @@ public class Drone implements StandardDrone {
 	 private int x;
 	 private int y;
 	 private int z;
+	 private int[] boundaries ;
+	 private int[] coordinates ;
+	//private int distanceFromOuterBox =10;
 	public Drone(){
 		
 	}
@@ -12,53 +15,64 @@ public class Drone implements StandardDrone {
 		this.y=y;
 		this.z=z;
 	}
+	public Drone(int[] boundaries, int[] coordinates){
+		this.boundaries = boundaries;
+		//distanceFromOuterBox=10;
+		this.coordinates=coordinates;
+	}
 	
 	public String moveUp() {
-		setY(y+1);
-		getFormatedCoordinates();
-		return x + ","+ y + "," + z;
-		
+		if(y<10){
+			setY(y+1);
+			getFormatedCoordinates();
+			return "Drone position: (" + Integer.toString(x) + ","+ Integer.toString(y) + "," + Integer.toString(z) + ")";
+		}
+		else
+		{
+			System.out.println("Ne moze gore!");
+			return "Ne moze gore!";
+		}
 	}
 
 	@Override
 	public String moveDown() {
 		setY(y-1);
 		getFormatedCoordinates();
-		return x + ","+ y + "," + z;
+		return "Drone position: (" + x + ","+ y + "," + z + ")";
 	}
 
 	@Override
 	public String moveLeft() {
 		setX(x - 1);
 		getFormatedCoordinates();
-		return x + ","+ y + "," + z;
+		return "Drone position: (" + x + ","+ y + "," + z + ")";
 	}
 
 	@Override
 	public String moveRight() {
 		setX(x + 1);
 		getFormatedCoordinates();
-		return x + ","+ y + "," + z;
+		return "Drone position: (" + x + ","+ y + "," + z + ")";
 	}
 
 	@Override
 	public String moveBack() {
 		setZ(z+1);
 		getFormatedCoordinates();
-		return x + ","+ y + "," + z;
+		return "Drone position: (" + x + ","+ y + "," + z + ")";
 	}
 
 	@Override
 	public String moveForth() {
 		setZ(z-1);
 		getFormatedCoordinates();
-		return x + ","+ y + "," + z;
+		return "Drone position: (" + x + ","+ y + "," + z + ")";
 	}
 
 	@Override
 	public String getFormatedCoordinates() {
-		System.out.println(x + ","+ y + "," + z);
-		return x + ","+ y + "," + z;
+		System.out.println("Drone position: (" + x + ","+ y + "," + z + ")");
+		return "Drone position: (" + Integer.toString(x) + ","+ Integer.toString(y) + "," + Integer.toString(z) + ")";
 	}
 	public int getX() {
 		return x;

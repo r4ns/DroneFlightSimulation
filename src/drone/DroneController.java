f3472c7b6d7eaeabee26af0daeebc145a1a32665
Vuk DragicevicIT14/2015 <dragicevicvuk10@gmail.com>
2017-03-23 12:19:56 +0100

    Vuk Dragicevic IT14/2015

--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -3,7 +3,46 @@ package drone;
 public class DroneController {
 
 	public static void main(String[] args) {		
-
-
+		
+Drone dron = new Drone(30,0,30);
+		
+		for (int i = 1; i<6; i++)
+		{
+			dron.moveUp();
+		}
+		System.out.println();
+		for (int i = 1; i<16; i++)
+		{
+			dron.moveForth();
+		}
+		System.out.println();
+		
+		for (int i = 1; i<42; i++)
+		{
+			dron.moveUp();
+		}
+		System.out.println();
+		
+		for(int i = 1; i<40; i++)
+		{
+			dron.moveBack();
+		}
+		System.out.println();
+		for(int i = 1; i<17;i++)
+		{
+			dron.moveDown();
+		}
+		System.out.println();
+		for(int i = 1;i<25; i++)
+		{
+			dron.moveLeft();
+		}
+		for(int i=1; i<25;i++){
+			dron.moveForth();
+		}
+		System.out.println();
+		for(int i=1;i<7;i++){
+			dron.moveLeft();
+		}
 	}
 }

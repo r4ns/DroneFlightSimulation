6bb0903e59f7e295a9debcedd7efd49ab9f50bdb
nemanjaarsic <nemanja.arsic815@gmail.com>
2017-03-23 22:07:54 +0100

    DroneFlightSimulation

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,97 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone {
+	private int x;
+	private int y;
+	private int z;
+	
+	public Drone(){
+		x = 30;
+		y = 0;
+		z = 30;
+	}
+
+	@Override
+	public String moveUp() {
+		if(y >= 0 && y < 50){
+			if((x >= 0 && x <= 10)||(x >= 40 && x<= 50)||(z >= 0 && z <= 10)||(z >= 40 && z <= 50))
+				y++;
+			else if(y < 10)
+				y++;
+			else if(y >= 40)
+				y++;
+		}
+		return null;
+	}
+
+	@Override
+	public String moveDown() {
+		if(y > 0 && y <= 50){
+			if((x >= 0 && x <= 10)||(x >= 40 && x <= 50)||(z >= 0 && z <= 10)||(z >= 40 && z <= 50))
+				y--;
+			else if(y <= 10)
+				y--;
+			else if(y > 40)
+				y--;
+		}
+		return null;
+	}
+
+	@Override
+	public String moveLeft() {
+		if(x > 0 && x <= 50){
+			if((y >= 0 && y <= 10)||(y >= 40 && y <= 50)||(z >= 0 && z <= 10)||(z >= 40 && z <= 50))
+				x--;
+			else if(x <= 10)
+				x--;
+			else if(y > 40)
+				x--;
+		}
+		return null;
+	}
+
+	@Override
+	public String moveRight() {
+		if(x >= 0 && x < 50){
+			if((y >= 0 && y <= 10)||(y >= 40 && y <= 50)||(z >= 0 && z <= 10)||(z >= 40 && z <= 50))
+				x++;
+			else if(x < 10)
+				x++;
+			else if(y >= 40)
+				x++;
+		}
+		return null;
+	}
+
+	@Override
+	public String moveBack() {
+		if(z >= 0 && z < 50){
+			if((x >= 0 && x <= 10)||(x >= 40 && x <= 50)||(y >= 0 && y <= 10)||(y >= 40 && y <= 50))
+				z++;
+			else if(x < 10)
+				z++;
+			else if(y >= 40)
+				z++;
+		}
+		return null;
+	}
+
+	@Override
+	public String moveForth() {
+		if(z > 0 && z <= 50){
+			if((x >= 0 && x <= 10)||(x >= 40 && x <= 50)||(y >= 0 && y <= 10)||(y >= 40 && y <= 50))
+				z--;
+			else if(x <= 10)
+				z--;
+			else if(y > 40)
+				z--;
+		}
+		return null;
+	}
+
+	@Override
+	public String getFormatedCoordinates() {
+		return "Pozicija drona: "+Integer.toString(x)+", "+Integer.toString(y)+", "+Integer.toString(z);
+	}
 
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -1,9 +1,71 @@
 package drone;
 
-public class DroneController {
+public class DroneController{
 
-	public static void main(String[] args) {		
-
-
+	public static void main(String[] args) {
+		Drone d = new Drone();
+		
+		System.out.println("-----Dron se pomera gore-----");
+		for(int i = 0; i < 12;i++)
+		{
+			d.moveUp();
+			System.out.println(d.getFormatedCoordinates());
+		}
+		
+		System.out.println("-----Dron se pomera desno-----");
+		for(int i = 0; i < 23;i++)
+		{
+			d.moveRight();
+			System.out.println(d.getFormatedCoordinates());
+		}
+		
+		System.out.println("-----Dron se pomera gore-----");
+		for(int i = 0; i < 45; i++)
+		{
+			d.moveUp();
+			System.out.println(d.getFormatedCoordinates());
+		}
+		
+		System.out.println("-----Dron se pomera levo-----");
+		for(int i = 0; i < 30; i++)
+		{
+			d.moveLeft();
+			System.out.println(d.getFormatedCoordinates());
+		}
+		
+		System.out.println("-----Dron se pomera nazad-----");
+		for(int i = 0; i < 23; i++)
+		{
+			d.moveBack();
+			System.out.println(d.getFormatedCoordinates());
+		}
+		
+		System.out.println("-----Dron se pomera dole-----");
+		for(int i = 0; i < 43; i++)
+		{
+			d.moveDown();
+			System.out.println(d.getFormatedCoordinates());
+		}
+		
+		System.out.println("-----Dron se pomera napred-----");
+		for(int i = 0; i < 20; i++)
+		{
+			d.moveForth();
+			System.out.println(d.getFormatedCoordinates());
+		}
+		
+		System.out.println("-----Dron se pomera levo-----");
+		for(int i = 0; i < 22; i++)
+		{
+			d.moveLeft();
+			System.out.println(d.getFormatedCoordinates());
+		}
+		
+		System.out.println("-----Dron se pomera gore-----");
+		for(int i = 0; i < 23; i++)
+		{
+			d.moveUp();
+			System.out.println(d.getFormatedCoordinates());
+		}
 	}
 }

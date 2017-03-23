ccedf43c23f0cc248ee8e827e0ac05532a9dbbe0
aleksandarbabic <alekski64@icloud.com>
2017-03-23 23:54:07 +0100

    meh

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,63 +1,79 @@
 package drone;
 
-public class Drone implements StandardDrone{
-	
-	private int currentX;
-	private int currentY;
-	private int currentZ;
-	
+public class Drone implements StandardDrone {
+
+	private int currentX, currentY, currentZ;
+
 	public Drone() {
 		currentX = 30;
 		currentY = 0;
 		currentZ = 30;
 		System.out.println(getFormatedCoordinates());
 	}
-	
+
 	@Override
 	public String moveUp() {
-		if(currentY<10 && (currentX>=0 && currentX<=30))
+
+		if (currentY < 10 && (currentX >= 0 && currentX <= 30))
 			currentY++;
-		if(currentY<50 && (currentX >=40 && currentX<=50))
+		else if (currentY < 50 && (currentX >= 40 && currentX <= 50))
 			currentY++;
+		else if (currentX >= 22 && currentZ == 9 && currentY < 50)
+			currentY++;
+
 		return getFormatedCoordinates();
 	}
 
 	@Override
 	public String moveDown() {
-		if(currentY>0)
+		if (currentY > 30 && currentX == 0)
 			currentY--;
 		return getFormatedCoordinates();
 	}
 
 	@Override
 	public String moveLeft() {
-		if(currentX > 0)
+		if (currentX > 40 && currentY == 33 && currentZ == 14)
 			currentX--;
+		else if (currentX <= 40 && currentY == 33 && currentZ == 9 && currentX > 22)
+			currentX--;
+		else if (currentZ <= 30 && currentY <= 50 && (currentX > 0 && currentX < 40))
+			currentX--;
+
 		return getFormatedCoordinates();
 	}
 
 	@Override
 	public String moveRight() {
-		if(currentX>=30 && currentX<50)
+		if (currentX >= 30 && currentX < 50)
 			currentX++;
 		return getFormatedCoordinates();
 	}
 
 	@Override
 	public String moveBack() {
-		// TODO Auto-generated method stub
+		if (currentZ < 30 && currentX == 22 && currentY <= 50)
+			currentZ++;
 		return getFormatedCoordinates();
 	}
 
 	@Override
 	public String moveForth() {
-		// TODO Auto-generated method stub
+		if ((currentX >= 40 && currentX <= 50) && currentY == 33)
+			if (currentZ > 9)
+				currentZ--;
+
 		return getFormatedCoordinates();
 	}
 
 	@Override
 	public String getFormatedCoordinates() {
-		return "Drone position : (" +currentX+","+ currentY+","+currentZ + ")";
+		if (currentX == 0 && currentY == 30 && currentZ == 30)
+			return "Destination reached! Coordinates : (" + currentX + "," + currentY + "," + currentZ + ")";
+		else if (currentX == 30 && currentY == 0 && currentZ == 30)
+			return "Drone spawned! Coordinates : (" + currentX + "," + currentY + "," + currentZ + ")";
+		else
+			return "Drone position : (" + currentX + "," + currentY + "," + currentZ + ")";
 	}
 
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -2,14 +2,40 @@ package drone;
 
 public class DroneController {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		Drone mojDron = new Drone();
-		for(int i = 0;i<10;i++)
-		System.out.println(mojDron.moveUp());
-		for(int i = 0;i<45;i++)
-			System.out.println(mojDron.moveRight());
-		for(int i = 0;i<45;i++)
+		System.out.println("MOVING UP!");
+		for (int i = 0; i < 12; i++)
 			System.out.println(mojDron.moveUp());
-
+		System.out.println("MOVING RIGHT!");
+		for (int i = 0; i < 22; i++)
+			System.out.println(mojDron.moveRight());
+		System.out.println("MOVING UP!");
+		for (int i = 0; i < 23; i++)
+			System.out.println(mojDron.moveUp());
+		System.out.println("MOVING FORTH!");
+		for (int i = 0; i < 16; i++)
+			System.out.println(mojDron.moveForth());
+		System.out.println("MOVING LEFT!");
+		for (int i = 0; i < 12; i++)
+			System.out.println(mojDron.moveLeft());
+		System.out.println("MOVING FORTH!");
+		for (int i = 0; i < 6; i++)
+			System.out.println(mojDron.moveForth());
+		System.out.println("MOVING LEFT!");
+		for (int i = 0; i < 18; i++)
+			System.out.println(mojDron.moveLeft());
+		System.out.println("MOVING UP!");
+		for (int i = 0; i < 20; i++)
+			System.out.println(mojDron.moveUp());
+		System.out.println("MOVING BACK!");
+		for (int i = 0; i < 22; i++)
+			System.out.println(mojDron.moveBack());
+		System.out.println("MOVING LEFT!");
+		for (int i = 0; i < 25; i++)
+			System.out.println(mojDron.moveLeft());
+		System.out.println("MOVING DOWN!");
+		for (int i = 0; i < 20; i++)
+			System.out.println(mojDron.moveDown());
 	}
 }

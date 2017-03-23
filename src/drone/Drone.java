998f5e3c4ed6b644a01b32fba2cae0ad2d1ad34b
Nebojsa <Nebojsa@DESKTOP-K9E9O7U>
2017-03-23 20:02:59 +0100

    Final

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,261 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone {
+	private int x ;
+	private int y ;
+	private int z ;
+	
+	
+	
+	
+	Drone (){
+		
+	}
+	
+	Drone(int x ,int y ,int z){
+		this.x = x ;
+		this.y = y ;
+		this.z = z ;
+	}
+	
+	
+	
+	
+	public String moveUp(int m) {
+		
+			if(this.x >= 0 && this.x <=50 && this.z>=40 && this.z<=50 ||this.x >= 0 && this.x <=50 && this.z>=0 && this.z<=10 || this.z>=0 && this.z <=50 && this.x>=40 && this.x<=50 || this.z>=0 && this.z <=50 && this.x>=0 && this.x<=10 ){
+				for(int i = 0 ; i<m ; i++){
+				if(y>=0 && y<50){
+				this.y +=1 ;
+				System.out.println(getFormatedCoordinates());
+				}
+				}
+				System.out.println("Hitting the wall !!!");
+			}else if(this.x >=10 && this.x<=40 && this.z >=10 && this.z<=40  && this.y>=0 && this.y<10){
+				
+					for(int i = 0 ; i<m ; i++){
+						if(y>=0 && y<10){
+						this.y +=1 ;
+						System.out.println(getFormatedCoordinates());
+						}
+						
+						
+				}
+					System.out.println("Hitting the wall !!!");
+				}else if(this.x >=10 && this.x<=40 && this.z >=10 && this.z<=40 && y>=40 && y<50  ){
+					
+						for(int i = 0 ; i<m ; i++){
+							if(y>=40 && y<50){
+							this.y +=1 ;
+							System.out.println(getFormatedCoordinates());
+							}
+							
+					}
+						System.out.println("Hitting the wall !!!");
+			}else
+				System.out.println("Hitting the wall !!!");
+		
+		
+		return null ;
+}
+	
+	
+	public String moveDown(int m) {
+		
+		if(this.x >= 0 && this.x <=50 && this.z>=40 && this.z<=50 ||this.x >= 0 && this.x <=50 && this.z>=0 && this.z<=10 || this.z>=0 && this.z <=50 && this.x>=40 && this.x<=50 || this.z>=0 && this.z <=50 && this.x>=0 && this.x<=10 ){
+			for(int i = 0 ; i<m ; i++){
+			if(y>0 && y<=50){
+			this.y -=1 ;
+			System.out.println(getFormatedCoordinates());
+			}
+			}
+			System.out.println("Hitting the wall !!!");
+		}else if(this.x >=10 && this.x<=40 && this.z >=10 && this.z<=40  && this.y>0 && this.y<=10){
+			
+				for(int i = 0 ; i<m ; i++){
+					if(y>0 && y<=10){
+					this.y -=1 ;
+					System.out.println(getFormatedCoordinates());
+					}
+					
+					
+			}
+				System.out.println("Hitting the wall !!!");
+			}else if(this.x >=10 && this.x<=40 && this.z >=10 && this.z<=40 && y>40 && y<=50  ){
+				
+					for(int i = 0 ; i<m ; i++){
+						if(y>40 && y<=50){
+						this.y -=1 ;
+						System.out.println(getFormatedCoordinates());
+						}
+						
+				}
+					System.out.println("Hitting the wall !!!");
+		}else
+			System.out.println("Hitting the wall !!!");
+	
+	
+	return null ;
+	}
+
+	
+	public String moveLeft(int m) {
+		if(this.y >= 0 && this.y <=50 && this.z>=40 && this.z<=50 ||this.y >= 0 && this.y <=50 && this.z>=0 && this.z<=10 || this.z>=0 && this.z <=50 && this.y>=40 && this.y<=50 || this.z>=0 && this.z <=50 && this.y>=0 && this.y<=10 ){
+			for(int i = 0 ; i<m ; i++){
+			if(x>0 && x<=50){
+			this.x -=1 ;
+			System.out.println(getFormatedCoordinates());
+			}
+			}
+			System.out.println("Hitting the wall !!!");
+		}else if(this.x >=10 && this.x<=40 && this.z >=10 && this.z<=40  && this.y>0 && this.y<=10){
+			
+				for(int i = 0 ; i<m ; i++){
+					if(x>0 && x<=10){
+					this.x -=1 ;
+					System.out.println(getFormatedCoordinates());
+					}
+					
+					
+			}
+				System.out.println("Hitting the wall !!!");
+			}else if(this.x >=10 && this.x<=40 && this.z >=10 && this.z<=40 && y>40 && y<=50  ){
+				
+					for(int i = 0 ; i<m ; i++){
+						if(x>40 && x<=50){
+						this.x -=1 ;
+						System.out.println(getFormatedCoordinates());
+						}
+						
+				}
+					System.out.println("Hitting the wall !!!");
+		}else
+			System.out.println("Hitting the wall !!!");
+	
+	
+	return null ;
+		
+	}
+
+	
+	public String moveRight(int m) {
+		
+		if(this.y >= 0 && this.y <=50 && this.z>=40 && this.z<=50 ||this.y >= 0 && this.y <=50 && this.z>=0 && this.z<=10 || this.z>=0 && this.z <=50 && this.y>=40 && this.y<=50 || this.z>=0 && this.z <=50 && this.y>=0 && this.y<=10 ){
+			for(int i = 0 ; i<m ; i++){
+			if(x>=0 && x<50){
+			this.x +=1 ;
+			System.out.println(getFormatedCoordinates());
+			}
+			}
+			System.out.println("Hitting the wall !!!");
+		}else if(this.x >=10 && this.x<=40 && this.z >=10 && this.z<=40  && this.y>=0 && this.y<10){
+			
+				for(int i = 0 ; i<m ; i++){
+					if(x>=0 && x<10){
+					this.x +=1 ;
+					System.out.println(getFormatedCoordinates());
+					}
+					
+					
+			}
+				System.out.println("Hitting the wall !!!");
+			}else if(this.x >=10 && this.x<=40 && this.z >=10 && this.z<=40 && y>=40 && y<50  ){
+				
+					for(int i = 0 ; i<m ; i++){
+						if(x>=40 && x<50){
+						this.x +=1 ;
+						System.out.println(getFormatedCoordinates());
+						}
+						
+				}
+					System.out.println("Hitting the wall !!!");
+		}else
+			System.out.println("Hitting the wall !!!");
+	
+	
+	return null ;
+	}
+
+	
+	public String moveBack(int m) {
+		
+		if(this.x >= 0 && this.x <=50 && this.y>=40 && this.y<=50 ||this.x >= 0 && this.x <=50 && this.y>=0 && this.y<=10 || this.y>=0 && this.y <=50 && this.x>=40 && this.x<=50 || this.y>=0 && this.y <=50 && this.x>=0 && this.x<=10 ){
+			for(int i = 0 ; i<m ; i++){
+			if(z>=0 && z<50){
+			this.z +=1 ;
+			System.out.println(getFormatedCoordinates());
+			}
+			}
+			System.out.println("Hitting the wall !!!");
+		}else if(this.x >=10 && this.x<=40 && this.z >=10 && this.z<=40  && this.y>=0 && this.y<10){
+			
+				for(int i = 0 ; i<m ; i++){
+					if(z>=0 && z<10){
+					this.z +=1 ;
+					System.out.println(getFormatedCoordinates());
+					}
+					
+					
+			}
+				System.out.println("Hitting the wall !!!");
+			}else if(this.x >=10 && this.x<=40 && this.z >=10 && this.z<=40 && y>=40 && y<50  ){
+				
+					for(int i = 0 ; i<m ; i++){
+						if(z>=40 && z<50){
+						this.z +=1 ;
+						System.out.println(getFormatedCoordinates());
+						}
+						
+				}
+					System.out.println("Hitting the wall !!!");
+		}else
+			System.out.println("Hitting the wall !!!");
+	
+	
+	return null ;
+	}
+
+	
+	public String moveForth(int m) {
+		
+		if(this.x >= 0 && this.x <=50 && this.y>=40 && this.y<=50 ||this.x >= 0 && this.x <=50 && this.y>=0 && this.y<=10 || this.y>=0 && this.y <=50 && this.x>=40 && this.x<=50 || this.y>=0 && this.y <=50 && this.x>=0 && this.x<=10 ){
+			for(int i = 0 ; i<m ; i++){
+			if(z>0 && z<=50){
+			this.z -=1 ;
+			System.out.println(getFormatedCoordinates());
+			}
+			}
+			System.out.println("Hitting the wall !!!");
+		}else if(this.x >=10 && this.x<=40 && this.z >=10 && this.z<=40  && this.y>0 && this.y<=10){
+			
+				for(int i = 0 ; i<m ; i++){
+					if(z>0 && z<=10){
+					this.z -=1 ;
+					System.out.println(getFormatedCoordinates());
+					}
+					
+					
+			}
+				System.out.println("Hitting the wall !!!");
+			}else if(this.x >=10 && this.x<=40 && this.z >=10 && this.z<=40 && y>40 && y<=50  ){
+				
+					for(int i = 0 ; i<m ; i++){
+						if(z>40 && z<=50){
+						this.z -=1 ;
+						System.out.println(getFormatedCoordinates());
+						}
+						
+				}
+					System.out.println("Hitting the wall !!!");
+		}else
+			System.out.println("Hitting the wall !!!");
+	
+	
+	return null ;
+	}
+	
+	public String getFormatedCoordinates() {
+		return "Drone position : (" + x + "," + y + "," + z + ")" ;
+	}
 
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -4,6 +4,21 @@ public class DroneController {
 
 	public static void main(String[] args) {		
 
-
+		Drone mojDron = new Drone(30 ,0, 30);
+		System.out.println(mojDron.getFormatedCoordinates());
+		mojDron.moveUp(10);
+		mojDron.moveRight(20);
+		mojDron.moveUp(23);
+		mojDron.moveForth(16);
+		mojDron.moveLeft(10);
+		mojDron.moveForth(5);
+		mojDron.moveLeft(18);
+		mojDron.moveUp(17);
+		mojDron.moveBack(21);
+		mojDron.moveLeft(32);
+		mojDron.moveDown(20);
+		System.out.println("Destination reached !!!");
+		
+		
 	}
 }

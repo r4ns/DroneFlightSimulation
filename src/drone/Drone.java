27234684cc58a0146b6936667ffaaeb2f852e63f
ChristianKlecina <djfntt@yahoo.com>
2017-03-24 10:18:27 +0100

    Domaci.

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,101 +1,133 @@
 package drone;
 
 public class Drone implements StandardDrone {
-	private int x ;
-	private int y ;
-	private int z ;
+	private int x = 30;
+	private int y  = 0;
+	private int z  = 30;
 	
-	public Drone(int x, int y, int z){
-		this.x=x;
-		this.y=y;
-		this.z=z;
-	}
+	
 	
 
 
 
 	public String moveUp() {
-		if(y<10 && y<10 || y>40) {
-			y=y+1;	
-			return "pomerio se gore za 1 " + " vrednost y je = " + y ;
-		}
-		else 
-			return "ne moze";	
+		if(((x>=0 && x<=10) || (x>=40 && x<=50))  && ((z>=0 && z<=50) && (y>=0 && y<50)) ) {
+			y++;
+			return "Y je pomeren za 1" + getFormatedCoordinates();
+		} else if((x>=10&& x<=40) && (z>=10 && z<=40) && ((y>=0 && y<10) || (y>=40 && y<50))){
+			y++;
+			return "Y je pomeren za 1" + getFormatedCoordinates();
+		} else if((x>=10&& x<=40) && (y>=0 && y<50) && ((z>=0 && z<10) || (z>=40 && z<50)) ) {
+			y++;
+			return "Y je pomeren za 1" + getFormatedCoordinates();
+			
+		} else
+			return "Ne moguce je pomeriti dron na gore" + getFormatedCoordinates();
+		
+		
+		
+			
 	}
 
 
 	public String moveDown() {
-		if(y<50 || y>0 && y<40) {
-			y=y-1;	
-			return "pomerio se dole za 1 " + " vrednost y je = " + y ;
-		}
-		else 
-			return "ne moze";
+		if(((x>=0 && x<=10) || (x>=40 && x<=50))  && ((z>=0 && z<=50) && (y>0 && y<=50))) {
+			y--;
+			return "Y je pomeren za 1" + getFormatedCoordinates();
+		} else if((x>=10&& x<=40) && (z>=10&& z<=40) && ((y>0 && y<=10) || (y>40 && y<=50))){
+			y--;
+			return "Y je pomeren za 1" + getFormatedCoordinates();
+		} else if ((x>=10 && x<=40) && (y>10 && y<=40) && ((z>=0 && z<10) || (z>=40 && z<50))) {
+			y--;
+			return "Y je pomeren za 1" + getFormatedCoordinates();
+			
+		} else
+			return "Ne moguce je pomeriti dron na dole" + getFormatedCoordinates();
+		
+		
 	}
 
 
 	@Override
 	public String moveLeft() {
-		if(y>0 && y<10){
-			x=x+1;
-			return "dron je pomeren u levo za 1" + "vrednost x = " + x; 
+		if(((y>=0 && y<=10) || (y>=40 && y<=50)) && (x>0 && x<=50) && (z>=0 && z<=50)){
+			x--;
+			return "X je pomeren za 1" + getFormatedCoordinates();
 		}
-		
-		else if(x>0 && x<10 || x>40 && x<50){
-			x=x+1;
-			return "dron je pomeren u levo za 1" + "vrednost x = " + x;
-		}
-		
-		else 
-			return "nije moguce pomeriti dron";
+		else if ((y>=10 && y<=40) && ((z>=0 && z<=10) || (z>=40 && z<=50)) && (x>0 && x<=50) ){
+			x--;
+			return "X je pomeren za 1" + getFormatedCoordinates();
+		} else if((y>=10 && y<=40) && (z>=10 && z<=40) && ((x>0 && x<=10) || (x>40 && x<=50) )){
+			x--;
+			return "X je pomeren za 1" + getFormatedCoordinates();
+			
+		} else{
+			return "Nije moguce pomeriti dron"; }
 	}
 
 
 
 	@Override
 	public String moveRight() {
-		if(y>0 && y<10){
-			x=x-1;
-			return "dron je pomeren u desno za 1" + "vrednost x = " + x; 
+		if(((y>=0 && y<=10) || (y>=40 && y<=50)) && (x>=0 && x<50) && (z>=0 && z<=50)){
+			x++;
+			return "X je pomeren za 1" + getFormatedCoordinates();
 		}
-		
-		else if(x>0 && x<10 || x>40 && x<50){
-			x=x-1;
-			return "dron je pomeren u desno za 1" + "vrednost x = " + x;
-		}
-		
-		else 
-			return "nije moguce pomeriti dron";
+		else if ((y>=10 && y<=40) && ((z>=0 && z<=10) || (z>=40 && z<=50)) && (x>=0 && x<50) ){
+			x++;
+			return "X je pomeren za 1" + getFormatedCoordinates();
+		} else if((y>=10 && y<=40) && (z>=0 && z<=50) && ((x>=0 && x<10) || (x>=40 && x<50) )){
+			x++;
+			return "X je pomeren za 1" + getFormatedCoordinates();
+			
+		} else{
+			return "Nije moguce pomeriti dron"; }
 	}
+	
 
 
 
 
 	@Override
 	public String moveBack() {
-		if(y>0 && y<10 && z<50 && z>0){
-			z=z+1;
-			return "pomeren je nazad, vrednost z = " + z;
-		}
-		else if (y>40 && y<50 && z<50 && z>0){
-			
-		}
-		return null;
+		if((z>=0 && z<10) || (z>=40 && z<50) && (x>=0 && x<=50) && (y>=0 && y<=50)) {
+			z++;
+			return "Z je pomeren za 1" + getFormatedCoordinates();
+		} else if((z>=10 && z<40) && ((x>= 0 && x<=10) || (x>= 40 && x<=50) ) && (y>=0 && y<=50) ){
+			z++;
+			return "Z je pomeren za 1" + getFormatedCoordinates();
+		} else if ((z>=10 && z<40) && ((y>= 0 && y<=10) || (y>= 40 && y<=50) ) && (x>=0 && x<=50) ) {
+			z++;
+			return "Z je pomeren za 1" + getFormatedCoordinates();
+		} else
+			return "Nije moguce pomeriti dron";
 	}
 
 
 	@Override
 	public String moveForth() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if((z>0 && z<=10) || (z>40 && z<=50) && (x>=0 && x<=50) && (y>=0 && y<=50)) {
+			z--;
+			return "Z je pomeren za 1" + getFormatedCoordinates();
+		} else if((z>10 && z<=40) && ((x>= 0 && x<=10) || (x>= 40 && x<=50) ) && (y>=0 && y<=50) ){
+			z--;
+			return "Z je pomeren za 1" + getFormatedCoordinates();
+		} else if ((z>10 && z<=40) && ((y>= 0 && y<=10) || (y>= 40 && y<=50) ) && (x>=0 && x<=50) ) {
+			z--;
+			return "Z je pomeren za 1" + getFormatedCoordinates();
+		} else
+			return "Nije moguce pomeriti dron";
+		
+		
 	}
 
 
 
 	@Override
 	public String getFormatedCoordinates() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return "Coordinates are: x " + x + "y " + y + "z " + z  ;
 	}
 	
 	
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -3,7 +3,43 @@ package drone;
 public class DroneController {
 
 	public static void main(String[] args) {		
-
+		Drone d1 = new Drone();
+		
+		System.out.println(d1.getFormatedCoordinates());
+		System.out.println("Pomera se gore");
+		for(int i=0;i<12;i++)
+			System.out.println(d1.moveUp());
+		System.out.println("Pomera se desno");
+		for(int i=0;i<22;i++)
+			System.out.println(d1.moveRight());
+		System.out.println("Pomera se gore");
+		for(int i=0;i<23;i++)
+			System.out.println(d1.moveUp());
+		System.out.println("Pomera se napred");
+		for(int i=0;i<16;i++)
+			System.out.println(d1.moveForth());
+		System.out.println("Pomera se levo");
+		for(int i=0;i<12;i++)
+			System.out.println(d1.moveLeft());
+		System.out.println("Pomera se napred");
+		for(int i=0;i<5;i++)
+			System.out.println(d1.moveForth());
+		System.out.println("Pomera se levo");
+		for(int i=0;i<18;i++)
+			System.out.println(d1.moveLeft());
+		System.out.println("Pomera se gore");
+		for(int i=0;i<17;i++)
+			System.out.println(d1.moveUp());
+		System.out.println("Pomera se nazad");
+		for(int i=0;i<21;i++)
+			System.out.println(d1.moveBack());
+		System.out.println("Pomera se levo");
+		for(int i=0;i<22;i++)
+			System.out.println(d1.moveLeft());
+		System.out.println("Pomera se dole");
+		for(int i=0;i<19;i++)
+			System.out.println(d1.moveDown());
+		System.out.println(d1.moveDown()+"Dron je stigao na cilj");
 
 	}
 }

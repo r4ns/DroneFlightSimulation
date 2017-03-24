ae81fe496e9c2f6ee53747b85ff49f7d21ca3dc5
itc1 <itc1@ws15.itc03.edu>
2017-03-24 20:53:31 +0100

    Vezbe 3

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -5,6 +5,16 @@ public class Drone implements StandardDrone {
 	int y = 0;
 	int z = 30;
 	
+	public Drone() {
+		
+	}
+	
+	
+	public Drone (int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
 	
 	public String moveUp() {
 			if ((y < 10 || (y >= 40 & y < 50))) {
@@ -98,6 +108,30 @@ public class Drone implements StandardDrone {
 	@Override
 	public String getFormatedCoordinates() {
 		return "(" + x + ", " + y + ", " + z + ")";
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
 	}
 
 }
---
+++ src/tests/DroneBasicTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import drone.Drone;
+
+public class DroneBasicTest {
+	private int[] boundaries = {50, 50, 50};
+	private int[] startCoordinates = {30, 0, 30};
+	
+	Drone drone;
+	
+	@Test
+	public void testInitialMoveUp() {
+		drone = new Drone(30, 0, 30);
+		//drone.setX(startCoordinates[0]);
+		
+		String expectedCoordinates = "(" + Integer.toString(startCoordinates[0]) + 
+				", " + Integer.toString(startCoordinates[1] + 1) +
+				", " + Integer.toString(startCoordinates[2]) + ")";
+		
+		assertEquals(expectedCoordinates, drone.moveUp());
+	}
+}

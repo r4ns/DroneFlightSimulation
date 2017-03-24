bcc6262df1ef3214d710f7f5e81ed3ff32da8ba2
it54g2015 <it54g2015@ws4.itc03.edu>
2017-03-24 13:07:10 +0100

    Drone II

--- src/tests/DroneBasicTests.java
+++ src/tests/DroneBasicTests.java
@@ -8,6 +8,7 @@ import drone.*;
 public class DroneBasicTests {
 	private int [] boundaries = {50,50,50};
 	private int [] startCoordinates={30,0,30};
+	private int [] brojIteracija;
 	Drone dron;
 	
 	@Test()//u zagradi mu definisemo vreme koje je neophodno da bi se test izvrsio
@@ -18,15 +19,17 @@ public class DroneBasicTests {
 		dron.setY(0);
 		dron.setZ(30);
 		
-		String expectedCoordinates = "Drone position: ("+Integer.toString(startCoordinates[0])+","+Integer.toString(startCoordinates[1]+1)+","+Integer.toString(startCoordinates[2])+")";
-		assertEquals(expectedCoordinates, dron.moveUp());
+		String expectedCoordinates = "Drone position: ("+Integer.toString(startCoordinates[0])+","+Integer.toString(startCoordinates[1]+brojIteracija[12])+","+Integer.toString(startCoordinates[2])+")";
+		//assertEquals(testInitialMoveUpIteration(expectedCoordinates, dron.moveUp()));
 		
 	}
-	@Test
-	public void testInitialMoveUpIteration(int [] startCoordinates){
-		for(int i = 0; i < 12;i++){
-			startCoordinates[1]+=i;
+	//@Test
+	/*public String testInitialMoveUpIteration(String ocekujemKoordinate,String vracamKoordinate, int [] brojPenjanja){
+		for(int i=1;i<=brojPenjanja.length;i++){
+			return Integer.parseInt(ocekujemKoordinate), dron.moveUp();
+			
 		}
-		
 	}
+*/
+	
 }

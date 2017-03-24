2ad9933d45c6d02109c4400f6cafdb36e06fb70f
itc1 <itc1@ws15.itc03.edu>
2017-03-24 20:55:29 +0100

    Vezbe 3

--- src/tests/DroneAdvancedTest.java
+++ src/tests/DroneAdvancedTest.java
@@ -1,5 +1,32 @@
 package tests;
 
-public class DroneAdvancedTest {
+import org.junit.Test;
 
+import drone.Drone;
+
+public class DroneAdvancedTest {
+	private int[] boundaries = {50, 50, 50};
+	private int[] startCoordinates = {30, 0, 30};
+	private int[] finishCoordinates = {0, 30, 30};
+	Drone drone;
+	
+	private String[] strings = {"up", "right", "up", "forth", "left", "forth", "left", "up", "back", "left", "down"};
+	
+	private int[] freq = {12, 21, 23, 16, 12, 5, 18, 17, 21, 22, 20};
+	
+	/*@Test
+	public void testFinishPosition() {
+		drone = new Drone(30, 0, 30);
+		
+		String expectedCoordinates = "(" + Integer.toString(finishCoordinates[0]) + 
+				", " + Integer.toString(finishCoordinates[1]) +
+				", " + Integer.toString(finishCoordinates[2]) + ")";
+		
+		public String helperTestFinishPosition() {
+			for (int i = 0; i < 11; i++)
+			{
+			}
+		}
+	
+	}*/
 }
--- src/tests/DroneTestSuite.java
+++ src/tests/DroneTestSuite.java
@@ -1,5 +1,14 @@
 package tests;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses ({
+	DroneBasicTest.class,
+	DroneAdvancedTest.class
+})
 public class DroneTestSuite {
 
 }

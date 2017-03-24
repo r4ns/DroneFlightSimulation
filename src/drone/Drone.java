558616054f81031a3ee2f5d1ac87c824b0b97ce9
it15g2015 <it15g2015@ws9.itc03.edu>
2017-03-24 20:47:33 +0100

    Zadatak2

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -12,6 +12,10 @@ public class Drone implements StandardDrone {
 		y=j;
 		z=k;
 	}
+	
+	public Drone(){
+		
+	}
 
 	@Override
 	public String moveUp() {
@@ -124,5 +128,31 @@ public class Drone implements StandardDrone {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
+	}
+	
+	
 	
 }

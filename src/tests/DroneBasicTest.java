8992882d70a71c51f097814a89a6eff4729f9192
itc1 <itc1@ws15.itc03.edu>
2017-03-24 20:57:41 +0100

    Vezbe3

--- src/tests/DroneBasicTest.java
+++ src/tests/DroneBasicTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import drone.Drone;
 
 public class DroneBasicTest {
-	private int[] boundaries = {50, 50, 50};
+	private int[] boundarie = {50, 50, 50};
 	private int[] startCoordinates = {30, 0, 30};
 	
 	Drone drone;

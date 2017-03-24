7d35af934cdc6811fc143096ec560ece3e64fc6d
IT10g2015 <IT10g2015@WS3.itc03.edu>
2017-03-24 20:47:51 +0100

    poyy

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -6,10 +6,14 @@ public class Drone implements StandardDrone{
 	int y;
 	int z;
 	
-	Drone(int x,int y,int z){
-		this.x=x;
-		this.y=y;
-		this.z=z;
+	public Drone(){
+		
+	}
+	
+	public Drone(int x,int y,int z){
+		this.x=30;
+		this.y=0;
+		this.z=30;
 	}
 	
 	public String moveUp(){
@@ -56,6 +60,30 @@ public class Drone implements StandardDrone{
 	}
 	public String getFormatedCoordinates(){
 		return "Drone position(x:"+this.x+" y:"+this.y+" z:"+this.z+")";
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
 	}
 
 }

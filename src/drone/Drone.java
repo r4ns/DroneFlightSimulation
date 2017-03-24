b1797c3ae11ab85130f74b8b0c3f101aba3eedfb
Korisnik <Korisnik@Asus-Laptop>
2017-03-24 12:21:09 +0100

    Commit hehe

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,98 @@
 package drone;
 
-public class Drone {
+public class Drone implements StandardDrone{
+	
+	int currentX;
+	int currentY;
+	int currentZ;
+	
+	public Drone(int x, int y, int z)
+	{
+		setCurrentX(x);
+		setCurrentY(y);
+		setCurrentZ(z);		
+	}
+	
+	
+	@Override
+	public String moveUp() {
+		// TODO Auto-generated method stub
+		setCurrentY(getCurrentY() + 1);
+		return "Moving Up";
+	}
 
+	@Override
+	public String moveDown() {
+		// TODO Auto-generated method stub
+		setCurrentY(getCurrentY() - 1);
+		return "Moving Down";
+	}
+
+	@Override
+	public String moveLeft() {
+		// TODO Auto-generated method stub
+		setCurrentX(getCurrentX() - 1);
+		return "Moving Left";
+	}
+
+	@Override
+	public String moveRight() {
+		// TODO Auto-generated method stub
+		setCurrentX(getCurrentX() + 1);
+		return "Moving Right";
+	}
+
+	@Override
+	public String moveBack() {
+		// TODO Auto-generated method stub
+		setCurrentZ(getCurrentZ() + 1);
+		return "Moving Back";
+	}
+
+	@Override
+	public String moveForth() {
+		// TODO Auto-generated method stub
+		setCurrentZ(getCurrentZ() - 1);
+		return "Moving Forth";
+	}
+
+	@Override
+	public String getFormatedCoordinates() {
+		// TODO Auto-generated method stub
+		return "Drone position: (" + getCurrentX() + "," + getCurrentY() + "," + getCurrentZ() + ")";
+	}
+
+	//////////////////////////GETTERS AND SETTERS////////////////////////////////
+	
+	
+	public int getCurrentX() {
+		return currentX;
+	}
+
+
+	public void setCurrentX(int currentX) {
+		this.currentX = currentX;
+	}
+
+
+	public int getCurrentY() {
+		return currentY;
+	}
+
+
+	public void setCurrentY(int currentY) {
+		this.currentY = currentY;
+	}
+
+
+	public int getCurrentZ() {
+		return currentZ;
+	}
+
+
+	public void setCurrentZ(int currentZ) {
+		this.currentZ = currentZ;
+	}
+
+	
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -4,6 +4,60 @@ public class DroneController {
 
 	public static void main(String[] args) {		
 
-
+		Drone dron = new Drone(30,0,30);
+		
+		for (int i = 0; i < 15; i++)
+		{
+			System.out.println(dron.moveRight());
+			System.out.println(dron.getFormatedCoordinates()); //0,45,30
+		}
+		
+		for (int i = 0; i < 45; i++)
+		{
+			System.out.println(dron.moveUp());
+			System.out.println(dron.getFormatedCoordinates()); //45,45,30
+		}
+		
+		for (int i = 0; i < 15; i++)
+		{
+			System.out.println(dron.moveLeft());
+			System.out.println(dron.getFormatedCoordinates()); //30,45,30
+		}
+		
+		for (int i = 0; i < 25; i++)
+		{
+			System.out.println(dron.moveForth());
+			System.out.println(dron.getFormatedCoordinates()); //30,45,5
+		}
+		
+		for (int i = 0; i < 15; i++)
+		{
+			System.out.println(dron.moveLeft());
+			System.out.println(dron.getFormatedCoordinates()); //15,45,5
+		}
+		
+		for (int i = 0; i < 40; i++)
+		{
+			System.out.println(dron.moveDown());
+			System.out.println(dron.getFormatedCoordinates()); //15,5,5
+		}
+		
+		for (int i = 0; i < 15; i++)
+		{
+			System.out.println(dron.moveLeft());
+			System.out.println(dron.getFormatedCoordinates()); //0,5,5
+		}
+		
+		for (int i = 0; i < 25; i++)
+		{
+			System.out.println(dron.moveBack());
+			System.out.println(dron.getFormatedCoordinates()); //0,5,30
+		}
+		
+		for (int i = 0; i < 25; i++)
+		{
+			System.out.println(dron.moveUp());
+			System.out.println(dron.getFormatedCoordinates()); //0,30,30
+		}				
 	}
 }

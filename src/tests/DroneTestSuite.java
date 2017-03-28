754969491699878c7305c78c397d01e3238c6ab4
aleksandarbabic <alekski64@icloud.com>
2017-03-28 14:34:01 +0200

    Implementiran test suite i logika za test runner-a sa ispisom na konzoli

--- src/tests/DroneTestSuite.java
+++ src/tests/DroneTestSuite.java
@@ -1,5 +1,12 @@
 package tests;
 
+import org.junit.runner.*;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({ DroneTestBasic.class, DroneTestAdvanced.class })
+
 public class DroneTestSuite {
 
 }

c6e4ea784fb39a10d4bdea4bb1b74d024909dab2
Ozren <Ozren@192.168.0.10>
2017-03-29 16:17:20 +0200

    Ozrensky IT37-2015

--- src/tests/DroneBasicTest.java
+++ src/tests/DroneBasicTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 import drone.Drone;
 
 public class DroneBasicTest {
-	private int[] boundaries = {50, 50, 50};
+	//private int[] boundaries = {50, 50, 50};
 	private int[] startCoordinates = {30, 0, 30};
 	Drone drone;
 	
@@ -82,5 +82,93 @@ public class DroneBasicTest {
 		assertEquals(expectedCoordinates, drone.moveBack());
 	}
 	
+	@Test
+	public void testUpperBoundaries(){
+		drone = new Drone();
+		//drone.setBoundaries(boundaries);
+		String expectedCoordinates = "Drone position: (" 
+				+ Integer.toString(startCoordinates[0]) + ", "
+				+ Integer.toString(startCoordinates[1] + 10) + ", "
+				+ Integer.toString(startCoordinates[2]) + ")";
+		for (int i = 0; i < 12; i++){
+			drone.moveUp();
+		}
+		
+		assertEquals(expectedCoordinates, drone.getFormatedCoordinates());
+	}
 	
+	@Test
+	public void testBottomBoundaries(){
+		drone = new Drone();
+		//drone.setBoundaries(boundaries);
+		String expectedCoordinates = "Drone position: (" 
+				+ Integer.toString(startCoordinates[0]) + ", "
+				+ Integer.toString(startCoordinates[1]) + ", "
+				+ Integer.toString(startCoordinates[2]) + ")";
+		for (int i = 0; i < 5; i++){
+			drone.moveDown();
+		}
+		
+		assertEquals(expectedCoordinates, drone.getFormatedCoordinates());
+	}
+	
+	@Test
+	public void testRightBoundaries(){
+		drone = new Drone();
+		//drone.setBoundaries(boundaries);
+		String expectedCoordinates = "Drone position: (" 
+				+ Integer.toString(startCoordinates[0] + 20) + ", "
+				+ Integer.toString(startCoordinates[1]) + ", "
+				+ Integer.toString(startCoordinates[2]) + ")";
+		for (int i = 0; i < 22; i++){
+			drone.moveRight();
+		}
+		
+		assertEquals(expectedCoordinates, drone.getFormatedCoordinates());
+	}
+	
+	@Test
+	public void testLeftBoundaries(){
+		drone = new Drone();
+		//drone.setBoundaries(boundaries);
+		String expectedCoordinates = "Drone position: (" 
+				+ Integer.toString(startCoordinates[0] - 30) + ", "
+				+ Integer.toString(startCoordinates[1]) + ", "
+				+ Integer.toString(startCoordinates[2]) + ")";
+		for (int i = 0; i < 32; i++){
+			drone.moveLeft();
+		}
+		
+		assertEquals(expectedCoordinates, drone.getFormatedCoordinates());
+	}
+	
+	@Test
+	public void testForthBoundaries(){
+		drone = new Drone();
+		//drone.setBoundaries(boundaries);
+		String expectedCoordinates = "Drone position: (" 
+				+ Integer.toString(startCoordinates[0]) + ", "
+				+ Integer.toString(startCoordinates[1]) + ", "
+				+ Integer.toString(startCoordinates[2] - 30) + ")";
+		for (int i = 0; i < 32; i++){
+			drone.moveForth();
+		}
+		
+		assertEquals(expectedCoordinates, drone.getFormatedCoordinates());
+	}
+	
+	@Test
+	public void testBackBoundaries(){
+		drone = new Drone();
+		//drone.setBoundaries(boundaries);
+		String expectedCoordinates = "Drone position: (" 
+				+ Integer.toString(startCoordinates[0]) + ", "
+				+ Integer.toString(startCoordinates[1]) + ", "
+				+ Integer.toString(startCoordinates[2] + 20) + ")";
+		for (int i = 0; i < 22; i++){
+			drone.moveBack();
+		}
+		
+		assertEquals(expectedCoordinates, drone.getFormatedCoordinates());
+	}
 }

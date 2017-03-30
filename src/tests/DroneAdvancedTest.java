7a81c843e3a39d368ec26655dc2b20c32f4a7cc8
AsusT100 <AsusT100@Asus>
2017-03-30 16:28:03 +0200

    tests v2

--- src/tests/DroneAdvancedTest.java
+++ src/tests/DroneAdvancedTest.java
@@ -14,8 +14,8 @@ public class DroneAdvancedTest {
 	public void test()
 	{	
 		Drone drone=new Drone(startCoordinates[0],startCoordinates[1],startCoordinates[2]);
-		String[] niz={"moveUp()-12","moveRight()-21","moveUp()-23","moveForth()-16","moveLeft()-12","moveForth()-5","moveLeft()-18","moveUp()-17","moveBack()-21","moveLeft()-22","moveDown()-20"};
-		String expectedCoordinates="Drone position- (" + Integer.toString(endCoordinates[0])+","+ Integer.toString(endCoordinates[1]) +","+ Integer.toString(endCoordinates[2])+")";
+		String[] niz={"moveUp():12","moveRight():21","moveUp():23","moveForth():16","moveLeft():12","moveForth():5","moveLeft():18","moveUp():17","moveBack():21","moveLeft():22","moveDown():20"};
+		String expectedCoordinates="Drone position: (" + Integer.toString(endCoordinates[0])+","+ Integer.toString(endCoordinates[1]) +","+ Integer.toString(endCoordinates[2])+")";
 		assertEquals(expectedCoordinates, helperMove(niz,drone));	
 	}
 	
@@ -23,7 +23,7 @@ public class DroneAdvancedTest {
 	{
 		
 		for(int i=0;i<niz.length;i++){
-			String[] part = niz[i].split("-");
+			String[] part = niz[i].split(":");
 			String func =part[0];
 			int number=Integer.parseInt(part[1]);
 			

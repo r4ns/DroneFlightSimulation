8fee597e21b23c89c47157eec2e32e27b30dca43
Comtrade <Comtrade@Comtrade-PC>
2017-03-30 12:38:38 +0200

    Updated

--- src/tests/DroneTestSuite.java
+++ src/tests/DroneTestSuite.java
@@ -1,5 +1,13 @@
 package tests;
 
+
+import org.junit.runner.*;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({ DroneTestBasic.class, DroneTestAdvanced.class })
+
 public class DroneTestSuite {
 
 }

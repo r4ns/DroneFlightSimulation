d35ed5a93f68e30feafc1bc539447a9540ecd576
Milos Covilo <miloscovilo@hotmail.com>
2017-03-30 15:37:11 +0200

    Proba

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,11 +1,12 @@
 package drone;
-
+import java.awt.SecondaryLoop;
+import java.lang.*;
 public class Drone implements StandardDrone {
 
 	public int x;
 	public int y;
 	public int z;
-	
+	public boolean poruka = false;
 	public Drone(int x,int y,int z){
 		this.x =x;
 		this.y =y;
@@ -16,34 +17,46 @@ public class Drone implements StandardDrone {
 	
 	@Override
 	public String moveUp() {
-		
-		this.y++;
+
+		UP_granica();
+		//if(poruka==true){
+		//	return "PROBIJANJE GRANICE";
+		//}
+		//poruka = false;
 		return "("+ this.x + "," + this.y + "," + this.z + ")";
 
 	}
 
 	@Override
 	public String moveDown() {
-		this.y--;
+		DOWN_granica();
+		//if(poruka==true){
+		//	return "PROBIJANJE GRANICE";
+		//}
+		//poruka = false;
 		return "("+ this.x + "," + this.y + "," + this.z + ")";
 	}
 
 	@Override
 	public String moveLeft() {
-		this.x--;
+		LEFT_granica();
+		//if(poruka==true){
+			//return "PROBIJANJE GRANICE";
+		//}
+		//poruka = false;		
 		return "("+ this.x + "," + this.y + "," + this.z + ")";
 	}
 
 	@Override
 	public String moveRight() {
-		this.x++;
+		RIGHT_granica();
 		return "("+ this.x + "," + this.y + "," + this.z + ")";
 	}
 
 	@Override
 	public String moveBack() {
 		
-		this.z++;
+		BACK_granica();
 		return "("+ this.x + "," + this.y + "," + this.z + ")";
 		
 	}
@@ -51,13 +64,98 @@ public class Drone implements StandardDrone {
 	@Override
 	public String moveForth() {
 		
-		this.z--;
+		FORTH_granica();
 		return "("+ this.x + "," + this.y + "," + this.z + ")";
 	}
 
 	@Override
 	public String getFormatedCoordinates() {
 		return "("+ this.x + "," + this.y + "," + this.z + ")";
+	}
+	
+	
+	
+	public void UP_granica(){
+
+		if(( x >= 10 && x <=40 ) && ( z >= 10 && z <= 40)){
+			if(y < 10 && y >= 0 || y >= 40 && y < 50){
+				y++;
+			}
+		}else if(y<50){
+				y++;
+		}else{
+			poruka=true;
+		}
+
+	}
+	
+	public void DOWN_granica(){
+		
+		if(( x >= 10 && x <=40 ) && ( z >= 10 && z <= 40)){
+			if(y <= 10 && y > 0 || y > 40 && y <= 50){
+				y--;
+			}
+		}else if(y<=50){
+			y--;
+		}else{
+			poruka=true;
+		}
+	}
+	
+	public void LEFT_granica(){
+		if((z<=10 && z>=0) || (z<=50 && z>=40)){
+			if(x>0){
+				x--;
+			}
+		}else if((z<=40 && z>=10)){
+			if((y<=10 && y>=0) || ( y >= 40 && y < 50)){
+				if(x>0){
+					x--;
+				}
+			}
+		}
+	}
+	
+	public void RIGHT_granica(){
+		if((z<=10 && z>=0) || (z<=50 && z>=40)){
+			if(x<50){
+				x++;
+			}
+		}else if((z<=40 && z>=10)){
+			if((y<=10 && y>=0) || ( y >= 40 && y < 50)){
+				if(x<50){
+					x++;
+				}
+			}
+		}
+	}
+	
+	public void BACK_granica(){
+		if((x<=10 && x>=0) || (x<=50 && x>=40)){
+			if(z<50){
+				z++;
+			}
+		}else if((x<=40 && x>=10)){
+			if((y<=10 && y>=0) || ( y >= 40 && y < 50)){
+				if(z<50){
+					z++;
+				}
+			}
+		}
+	}
+	
+	public void FORTH_granica(){
+		if((x<=10 && x>=0) || (x<=50 && x>=40)){
+			if(z>0){
+				z--;
+			}
+		}else if((x<=40 && x>=10)){
+			if((y<=10 && y>=0) || ( y >= 40 && y < 50)){
+				if(z>0){
+					z--;
+				}
+			}
+		}
 	}
 
 }
--- src/tests/DroneTestAdvanced.java
+++ src/tests/DroneTestAdvanced.java
@@ -9,16 +9,54 @@ import drone.Drone;
 public class DroneTestAdvanced {
 	private int[] boundaries = {50,50,50};
 	private int[] startCoordinates = {30,0,30};
+	private int[] finishCoordinates = {0,30,30};
 	Drone drone;
+	
+	private String []  direction = {"moveUp","moveRight",
+									"moveUp","moveForth",
+									"moveLeft","moveForth",
+									"moveLeft","moveUp",
+									"moveBack","moveLeft",
+									"moveDown"};
+	
+	private int [] steps = {12,21,23,16,12,5,18,17,21,22,20};
 	
 	
 	@Test
-	public void testTravelingDroneToFinalPosition()
+	public void testPath()
 	{
-		
-		String expectedCoordinates = "(30,0,30)";
-		
-		assertEquals(expectedCoordinates, drone.getFormatedCoordinates());
-		System.out.println(expectedCoordinates);
+		int n = steps.length;
+		for(int i = 0;i<n;i++){
+			if(direction[i]=="moveUp"){
+				for(int j=0;j<steps[i];j++){
+					drone.moveUp();
+				}
+			}
+			if(direction[i]=="moveDown"){
+				for(int j=0;j<steps[i];j++){
+					drone.moveDown();
+				}
+			}
+			if(direction[i]=="moveLeft"){
+				for(int j=0;j<steps[i];j++){
+					drone.moveLeft();
+				}
+			}
+			if(direction[i]=="moveRight"){
+				for(int j=0;j<steps[i];j++){
+					drone.moveUp();
+				}
+			}
+			if(direction[i]=="moveForth"){
+				for(int j=0;j<steps[i];j++){
+					drone.moveUp();
+				}
+			}
+			if(direction[i]=="moveBack"){
+				for(int j=0;j<steps[i];j++){
+					drone.moveUp();
+				}
+			}
+		}
 	}
 }

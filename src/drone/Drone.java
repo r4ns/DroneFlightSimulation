598c4af53fe16bb36a266a69794f32e102ab534e
repac <repac@aleks>
2017-03-30 12:03:05 +0200

    Aleksandra Repac IT31/2015

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,66 +1,137 @@
-package drone;
-
-public class Drone implements StandardDrone{
-
-	int x=30;
-	int y=0;
-	int z=30;
-	@Override
-	public String moveUp() {
+ package drone;
+  
+ public class Drone implements StandardDrone{
+ 	
+ 	private int[] boundaries={0,10,40,50};
+ 	public int[] coordinates={30,0,30};
+ 	
+ 	public Drone()
+ 	{
+ 		
+ 	}
+ 	
+ 	public Drone(int[] coordinates){
+ 		this.coordinates=coordinates;
+ 	}
+ 	
+ 	public Drone(int[] boundaries, int[] coordinates){
+ 		this.boundaries=boundaries;
+ 		this.coordinates=coordinates;
+ 	}
+ 	public String moveUp() {
+ 		// TODO Auto-generated method stub
+ 		if ((coordinates[0]>10 &&coordinates[0]<40) && (coordinates[2]>10 &&coordinates[2]<40) && coordinates[1]<10){
+ 			coordinates[1]++;
+ 			 			return getFormatedCoordinates();
+ 			 		} else if ((coordinates[0]>10 &&coordinates[0]<40) && (coordinates[2]>10 &&coordinates[2]<40) && (coordinates[1]>=40 && coordinates[1]<50)){
+ 			 			coordinates[1]++;
+ 			 			return getFormatedCoordinates();
+ 			 		} else if (((coordinates[0]>=40 || coordinates[0]<=10) || (coordinates[2]>=40 || coordinates[2]<= 10))  && coordinates[1]<50){
+ 			 			coordinates[1]++;
+ 			 			return getFormatedCoordinates();
+ 			 		} else {
+ 			 			
+ 			 			return getFormatedCoordinates();
+ 			 		}
+ 	}
+ 
+ @Override
+ 	public String moveDown() {
+ 		// TODO Auto-generated method stub
+	 if ((coordinates[0]>10 &&coordinates[0]<40) && (coordinates[2]>10 &&coordinates[2]<40) && coordinates[1]>0 && coordinates[1]<=10){
+			coordinates[1]--;
+			 			return getFormatedCoordinates();
+			 		} else if ((coordinates[0]>10 &&coordinates[0]<40) && (coordinates[2]>10 && coordinates[2]<40) && (coordinates[1]<=50 && coordinates[1]>40)){
+			 			coordinates[1]--;
+			 			return getFormatedCoordinates();
+			 		} else if (((coordinates[0]>=40 || coordinates[0]<= 10) || (coordinates[2]>=40 || coordinates[2]<= 10))  && coordinates[1]>0){
+			 			coordinates[1]--;
+			 			return getFormatedCoordinates();
+			 			
+			 		} else {
+			 			
+			 			return getFormatedCoordinates();
+			 		}
+	}
+ 	
+ 
+ 	@Override
+ 	public String moveLeft() {
+ 		// TODO Auto-generated method stub
+ 		
+ 		 if ((coordinates[1]>10 &&coordinates[1]<40) && (coordinates[2]>10 &&coordinates[2]<40) && (coordinates[0]<=10 && coordinates[0]>0)){
+ 			coordinates[0]--;
+ 			 			return getFormatedCoordinates();
+ 			 		} else if ((coordinates[1]>10 &&coordinates[1]<40) && (coordinates[2]>10 && coordinates[2]<40) && (coordinates[0]>40 && coordinates[0]<=50)){
+ 			 			coordinates[0]--;
+ 			 			return getFormatedCoordinates();
+ 			 		} else if (((coordinates[1]>=40 || coordinates[1]<= 10) || (coordinates[2]>=40 || coordinates[2]<= 10))  && coordinates[0]>0){
+ 			 			coordinates[0]--;
+ 			 			return getFormatedCoordinates();
+ 			 		} else {
+ 			 			return getFormatedCoordinates();
+ 			 		}
+ 		
+ 	}
+ 	
+ 	@Override
+ 	public String moveRight() {
+		 if ((coordinates[1]>10 &&coordinates[1]<40) && (coordinates[2]>10 &&coordinates[2]<40) && coordinates[0]<10){
+			coordinates[0]++;
+			 			return getFormatedCoordinates();
+			 		} else if ((coordinates[1]>10 &&coordinates[1]<40) && (coordinates[2]>10 &&coordinates[2]<40) && (coordinates[0]>=40 && coordinates[0]<50)){
+			 			coordinates[0]++;
+			 			return getFormatedCoordinates();
+			 		} else if (((coordinates[1]>=40 || coordinates[1]<= 10) || (coordinates[2]>=40 || coordinates[2]<= 10))  && coordinates[0]<50){
+			 			coordinates[0]++;
+			 			return getFormatedCoordinates();
+			 		} else {
+			 			return getFormatedCoordinates();
+			 		}
+		
+	}
+ 	
+ 		// TODO Auto-generated method stub
+ 		
+ 
+ 	@Override
+ 	public String moveBack() {
+ 		// TODO Auto-generated method stub
+ 		 if ((coordinates[0]>10 &&coordinates[0]<40) && (coordinates[1]>10 &&coordinates[1]<40) && coordinates[2]<10){
+ 			coordinates[2]++;
+ 			 			return getFormatedCoordinates();
+ 			 		} else if ((coordinates[0]>10 &&coordinates[0]<40) && (coordinates[1]>10 &&coordinates[1]<40) && (coordinates[2]>=40 && coordinates[2]<50)){
+ 			 			coordinates[2]++;
+ 			 			return getFormatedCoordinates();
+ 			 		} else if (((coordinates[0]>=40 || coordinates[0]<= 10) || (coordinates[1]>=40 || coordinates[1]<= 10))  && coordinates[2]<50){
+ 			 			coordinates[2]++;
+ 			 			return getFormatedCoordinates();
+ 			 		} else {
+ 			 			return getFormatedCoordinates();
+ 			 		}
+ 	
+ 	}
+ 
+ 	@Override
+ 	public String moveForth() {
+ 		if ((coordinates[0]>10 &&coordinates[0]<40) && (coordinates[1]>10 &&coordinates[1]<40) && (coordinates[2]>0 && coordinates[2]<=10)){
+ 			coordinates[2]--;
+ 			 			return getFormatedCoordinates();
+ 			 		} else if ((coordinates[0]>10 &&coordinates[0]<40) && (coordinates[1]>10 && coordinates[1]<40) && coordinates[2]>40){
+ 			 			coordinates[2]--;
+ 			 			return getFormatedCoordinates();
+ 			 		} else if (((coordinates[0]>=40 || coordinates[0]<= 10) || (coordinates[1]>=40 || coordinates[1]<= 10))  && coordinates[2]>0){
+ 			 			coordinates[2]--;
+ 			 			return getFormatedCoordinates();
+ 			 		} else {
+ 			 			return getFormatedCoordinates();
+ 			 		}
+ 	}
+ 
+ 	@Override
+ 	public String getFormatedCoordinates() {
 		// TODO Auto-generated method stub
-		y++;
-		
-		//System.out.println(x +","+y+","+z);
-		return getFormatedCoordinates();
+		return "("+coordinates[0]+","+coordinates[1]+","+coordinates[2]+")";
 	}
-
-	@Override
-	public String moveDown() {
-		// TODO Auto-generated method stub
-		y--;
-		
-		
-		return getFormatedCoordinates();
-	}
-
-	@Override
-	public String moveLeft() {
-		// TODO Auto-generated method stub
-		x--;
-		
-		return getFormatedCoordinates();
-	}
-
-	@Override
-	public String moveRight() {
-		x++;
-		
-		
-		return getFormatedCoordinates();
-	}
-
-	@Override
-	public String moveBack() {
-		// TODO Auto-generated method stub
-		z++;
-		return getFormatedCoordinates();
-	}
-
-	@Override
-	public String moveForth() {
-		// TODO Auto-generated method stub
-		z--;
-		
-		//System.out.println(x +","+y+","+z);
-		return getFormatedCoordinates();
-		
-	}
-
-	@Override
-	public String getFormatedCoordinates() {
-		// TODO Auto-generated method stub
-		System.out.println(x +","+y+","+z);
-		return null;
-	}
-
+  
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -2,66 +2,59 @@ package drone;
 
 public class DroneController {
 
-	public static void main(String[] args) {		
-
-		Drone drone = new Drone();
-		int i;
+	public static void main(String[] args) {
 		
-		System.out.println("Penjes se gore");
-		
-		for (i=drone.y; i<10; i++)
-		{
+		Drone drone= new Drone();
+		System.out.println("Dron ide gore!");
+		while(drone.coordinates[1]<10){
 			drone.moveUp();
+			System.out.println(drone.getFormatedCoordinates());
 		}
-		
-		System.out.println("Ides desno");
-		
-		for(i=drone.x; i<50; i++)
-		{
+		System.out.println("Pomeranje desno!");
+		while(drone.coordinates[0]<50){
 			drone.moveRight();
+			System.out.println(drone.getFormatedCoordinates());
 		}
-		
-		System.out.println("Penjes se gore");
-		
-		for (i=drone.y; i<50; i++)
-		{
+		System.out.println("Podizanje gore 2!");
+		while(drone.coordinates[1]<50){
 			drone.moveUp();
+			System.out.println(drone.getFormatedCoordinates());
 		}
-		
-		System.out.println("Ides napred i oblilazis drugu stranu kocke");
-		
-		for(i=drone.z; i<50; i++)
+		System.out.println("Drone ide napred");
+		while(drone.coordinates[2]>15)
 		{
 			drone.moveForth();
+			System.out.println(drone.getFormatedCoordinates());
+			
 		}
-		
-		System.out.println("Ides levo");
-		
-		for(i=drone.x; i>25; i--)
+		System.out.println("Drone ide levo");
+		while(drone.coordinates[0]>30)
 		{
 			drone.moveLeft();
+			System.out.println(drone.getFormatedCoordinates());
+			
 		}
-		
-		System.out.println("Na kocki si");
-		
-		for(i=drone.z; i<30; i++)
+		System.out.println("Drone je na kocki");
+		while(drone.coordinates[2]<30)
 		{
 			drone.moveBack();
+			System.out.println(drone.getFormatedCoordinates());
+			
 		}
-		
-		System.out.println("Ides levo do kraja kocke");
-		
-		for(i=drone.x; i>0; i--)
+		System.out.println("Drone ide opet levo");
+		while(drone.coordinates[0]>0)
 		{
 			drone.moveLeft();
+			System.out.println(drone.getFormatedCoordinates());
+			
 		}
-		
-		System.out.println("Spusta se na cilj");
-		
-		for(i=drone.y; i>30; i--)
+		System.out.println("Drone ide na cilj");
+		while(drone.coordinates[1]>30)
 		{
 			drone.moveDown();
+			System.out.println(drone.getFormatedCoordinates());
+			
 		}
 		
-}
+	}
 }

2c0ecbd77c651b5e6ebbf3de747c2604f6225ea4
čevu <evu@192.168.0.11>
2017-03-30 15:27:41 +0200

    IT14/2015

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,89 +1,188 @@
 package drone;
 
-public class Drone implements StandardDrone{
-	
-	private int x = 30;
-	private int y = 0;
-	private int z = 30;
-	
-	public Drone(){
-		
-	}
-	public Drone(int x, int y, int z){
-		
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
-	@Override
-	public String moveUp() {
-		y++;
-		return getFormatedCoordinates();
-	}
-
-	@Override
-	public String moveDown() {
-		y--;
-		return getFormatedCoordinates();
-	}
-
-	@Override
-	public String moveLeft() {
-		x--;
-		return getFormatedCoordinates();
-	}
-
-	@Override
-	public String moveRight() {
-		x++;
-		return getFormatedCoordinates();
-	}
-
-	@Override
-	public String moveBack() {
-		z--;
-		return getFormatedCoordinates();
-	}
-
-	@Override
-	public String moveForth() {
-		z++;
-		return getFormatedCoordinates();
-	}
-
-	@Override
-	public String getFormatedCoordinates() {
-		
-		System.out.println(("(" + this.x + "," + this.y + "," + this.z + ")"));
-		
-		return ("(" + this.x + "," + this.y + "," + this.z + ")") ;
-	}
-	public int getX() {
-		return x;
-	}
-
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-
-	public int getY() {
-		return y;
-	}
-
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
-
-	public int getZ() {
-		return z;
-	}
-
-
-	public void setZ(int z) {
-		this.z = z;
-	}
+public class Drone implements StandardDrone {
+  	
+ 	private int x = 30;
+ 	private int y = 0;
+ 	private int z = 30;
+  	
+  	private int [] outerBoundaries;
+  	private int distanceFromOuterBox=10;
+ 	private int [] coordinates1;
+ 	private int [] coordinates = {30,0,30};
+  	private int step=1;
+  	
+  	
+  	public Drone(){
+  		
+ 	}
+ 	public Drone(int x, int y, int z){
+ 		
+ 		this.x = x;
+ 		this.y = y;
+ 		this.z = z;
+ 	}
+ 	
+ 		
+  	public Drone(int [] boundaries, int [] coordinates)
+  	{
+  		this.outerBoundaries= boundaries;
+  		distanceFromOuterBox=10;
+  		this.coordinates1 = coordinates;
+  	}
+  	
+ 	
+ 
+ 	@Override
+  	public String moveUp() {
+ 		if (y<11)
+ 		{
+ 			y++;
+ 		}
+ 		else
+ 		{
+ 			System.out.println("Greska");
+ 		}
+ 		if(coordinates1[0] > 10 && coordinates1[0]< 40 && coordinates1[2] > 10 && coordinates1[2] < 40)
+ 			 		{
+ 			 			if(coordinates1[1] >= 0 && coordinates1[1] < 10 || coordinates1[1] >= 40 && coordinates1[1] < 50)
+ 			 			{
+ 			 				coordinates1[1]++;
+ 			 			}
+ 			 			
+ 			 		}
+ 			 		else if (coordinates1[1]>=0 && coordinates1[1] <50)
+ 			 			coordinates1[1]++;
+ 		
+  		
+  		return getFormatedCoordinates();
+  	}
+  
+  	@Override
+  	public String moveDown() {
+ 		y--;
+ 	if(coordinates1[0] > 10 && coordinates1[0] < 40 && coordinates1[2] > 10 && coordinates1[2] < 40)
+ 			 		{
+ 			 			if(coordinates1[1] <= 50 && coordinates1[1] > 40 || coordinates1[1] <= 10 && coordinates1[1] > 0)
+ 			 			{
+ 		 				coordinates1[1]--;
+ 			 			}
+			 			
+ 			 		}
+ 			 		else if (coordinates1[1] > 0 && coordinates1[1]<=50)
+ 			 			coordinates1[1]--;
+ 		
+  		return getFormatedCoordinates();
+  	}
+  
+  	@Override
+  	public String moveLeft() {
+ 		x--;
+ 		if(coordinates1[1] > 10 && coordinates1[1] < 40 && coordinates1[2] > 10 && coordinates1[2] < 40)
+ 			 		{
+ 		 			if(coordinates1[0] <= 50 && coordinates1[0] > 40 || coordinates1[0] <= 10 && coordinates1[0] > 0)
+ 			 			{
+ 			 				coordinates1[0]--;
+ 			 			}
+ 			 			
+ 			 		}
+ 					else if (coordinates1[0] > 0 && coordinates1[0]<=50)
+ 					coordinates1[0]--;
+ 		
+  		return getFormatedCoordinates();
+  	}
+  
+  	@Override
+  	public String moveRight() {
+ 		x++;
+ 		if(coordinates1[1] > 10 && coordinates1[1] < 40 && coordinates1[2] > 10 && coordinates1[2] < 40)
+ 			 		{
+ 			 			if(coordinates1[0] >= 0 && coordinates1[0] < 10 || coordinates1[0] >= 40 && coordinates1[0] < 50)
+ 			 			{
+ 			 				coordinates1[0]++;
+ 			 			}
+ 			 			
+ 			 		}
+ 			 		else if (coordinates1[0] >= 0 && coordinates1[0] < 50)
+ 			 			coordinates1[0]++;
+ 			 		
+ 		
+  		return getFormatedCoordinates();
+  	}
+  
+  	@Override
+  	public String moveBack() {
+ 		z--;
+ 		if(coordinates1[1] > 10 && coordinates1[1] < 40 && coordinates1[0] > 10 && coordinates1[0] < 40)
+ 			 		{
+ 			 			if(coordinates1[2] >= 0 && coordinates1[2] < 10 || coordinates1[2] >= 40 && coordinates1[2] < 50)
+ 			 			{
+ 			 				coordinates1[2]++;
+ 			 			}
+ 			 			
+ 			 		}
+ 			 		else if (coordinates1[2]>=0 && coordinates1[2] < 50)
+ 			 			coordinates1[2]++;
+  		return getFormatedCoordinates();
+  	}
+  
+  	@Override
+  	public String moveForth() {
+ 		z++;
+ 		if(coordinates1[1] > 10 && coordinates1[1] < 40 && coordinates1[0] > 10 && coordinates1[0] < 40)
+ 			 		{
+ 			 			if(coordinates1[2]<= 50 && coordinates1[2] > 40 || coordinates1[2] <= 10 && coordinates1[2] > 0)
+ 			 			{
+ 			 				coordinates1[2]--;
+ 			 			}
+ 			 			
+ 			 		}
+ 			 		else if (coordinates1[2] > 0 && coordinates1[2]<=50)
+ 			 			coordinates1[2]--;
+ 			 		
+  		return getFormatedCoordinates();
+  	}
+  
+  	@Override
+  	public String getFormatedCoordinates() {
+  		
+ 		System.out.println(("(" + this.x + "," + this.y + "," + this.z + ")"));
+ 		System.out.println("(" + Integer.toString(coordinates1[0]) +
+ 				"," + Integer.toString(coordinates1[1])  + 
+ 				"," + Integer.toString(coordinates1[2]) + ")");
+  		
+ 		return ("(" + Integer.toString(coordinates1[0]) +
+ 				"," + Integer.toString(coordinates1[1])  + 
+ 				"," + Integer.toString(coordinates1[2]) + ")");
+  	}
+  
+  
+ 	public int getX() {
+ 		return x;
+ 	}
+ 
+  
+ 	public void setX(int x) {
+ 		this.x = x;
+ 	}
+ 
+ 
+ 	public int getY() {
+ 		return y;
+ 	}
+ 
+ 
+ 	public void setY(int y) {
+ 		this.y = y;
+ 	}
+ 
+ 
+ 	public int getZ() {
+ 		return z;
+ 	}
+ 
+ 
+ 	public void setZ(int z) {
+ 		this.z = z;
+ 	}
 }
--- src/drone/DroneController.java
+++ src/drone/DroneController.java
@@ -1,48 +1,80 @@
 package drone;
 
 public class DroneController {
-
-	public static void main(String[] args) {		
+public static void main(String[] args) {	
 		
-Drone dron = new Drone(30,0,30);
-		
-		for (int i = 1; i<6; i++)
-		{
-			dron.moveUp();
-		}
-		System.out.println();
-		for (int i = 1; i<16; i++)
-		{
-			dron.moveForth();
-		}
-		System.out.println();
-		
-		for (int i = 1; i<42; i++)
-		{
-			dron.moveUp();
-		}
-		System.out.println();
-		
-		for(int i = 1; i<40; i++)
-		{
-			dron.moveBack();
-		}
-		System.out.println();
-		for(int i = 1; i<17;i++)
-		{
-			dron.moveDown();
-		}
-		System.out.println();
-		for(int i = 1;i<25; i++)
-		{
-			dron.moveLeft();
-		}
-		for(int i=1; i<25;i++){
-			dron.moveForth();
-		}
-		System.out.println();
-		for(int i=1;i<7;i++){
-			dron.moveLeft();
-		}
-	}
+	 		Drone dron = new Drone(30,0,30);
+	 		Drone dron1 = new Drone();
+	  		
+	 		System.out.println(">>>Moving up<<<");
+	  		for (int i = 1; i<5; i++)
+	  		{
+	  			dron1.moveUp();
+	  		}
+	  		System.out.println();
+	  		
+	 		System.out.println(">>>Moving right<<<");
+	  		for (int i = 1; i<13; i++)
+	  		{
+	  			dron1.moveRight();
+	  		}
+	  		System.out.println();
+	  		
+	 		System.out.println(">>>Moving up<<<");
+	  		for (int i = 1; i<42; i++)
+	  		{
+	  			dron1.moveUp();
+	  		}
+	  		System.out.println();
+	  		
+	 		System.out.println(">>>Moving left<<<");		
+	 		for(int i = 1; i<18; i++)
+	  		{
+	  			dron1.moveLeft();
+	  		}
+	  		System.out.println();
+	 		for(int i = 1; i<13;i++)
+	 		
+	 	System.out.println(">>>Moving forth<<<");
+	 		for(int i = 1; i<23;i++)
+	  		{
+	  			dron1.moveForth();
+	  		}
+	  		System.out.println();
+	 		for(int i = 1;i<16; i++)
+	 		
+	 		System.out.println(">>>Moving down<<<");
+	 		for(int i =1; i <16; i++)
+	  		{
+	  			dron1.moveDown();
+	  		}
+	  		System.out.println();
+	 		for(int i =1; i <23; i++)
+	 		
+	 		System.out.println(">>>Moving left<<<");
+	 		for (int i = 1; i<20; i++)
+	  		{
+	  			dron1.moveLeft();
+	  		}
+	  		System.out.println();
+	 		for (int i = 1; i<13; i++)
+	 		
+	 		System.out.println(">>>Moving back<<<");		
+	 		for (int i=1; i<23; i++)
+	  		{
+	  			dron1.moveBack();
+	  		}
+	  		System.out.println();
+	 		for(int i = 1; i<4;i++)
+	 		
+	 		System.out.println(">>>Moving left<<<");
+	 		for(int i = 1; i<7;i++)
+	  		{
+	  			dron1.moveLeft();
+	  		}
+	  		
+	 		
+	  
+	  
+	  	}
 }

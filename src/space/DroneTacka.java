1af2caa5e6d4a161672ca9208de9461cb5b5f4f4
StefanKeler <stefankeler96@gmail.com>
2017-03-31 20:29:56 +0200

    DoubleDrone tests

--- src/space/DroneTacka.java
+++ src/space/DroneTacka.java
@@ -3,14 +3,42 @@ package space;
 import drone.StandardDrone;
 
 
-public class DroneTacka extends DoubleCubeDrone implements StandardDrone {
-
+public class DroneTacka implements StandardDrone {
+	
+	FlySpace space = new FlySpace();
+	
+	static int brojTacaka = 0;
+	
+	private String idTacke;
+	
 	private int c[] = new int[3];
 
-	private String poruka = "";
-
+	public DroneTacka(){
+		brojTacaka++;
+		idTacke = "" + brojTacaka + ". ";
+	}
+	
+	
 	public DroneTacka(int c[]){
 		this.c = c;
+		brojTacaka++;
+		idTacke = "" + brojTacaka + ". ";
+
+	}
+	public DroneTacka(int c[], FlySpace space){
+		this.c = c;
+		this.space = space;
+		brojTacaka++;
+		idTacke = "" + brojTacaka + ". ";
+
+	}
+	
+	public DroneTacka(int x, int y, int z){
+		c[0] = x;
+		c[1] = y;
+		c[2] = z;
+		brojTacaka++;
+		idTacke = "" + brojTacaka + ". ";
 
 	}
 
@@ -45,115 +73,47 @@ public class DroneTacka extends DoubleCubeDrone implements StandardDrone {
 				}
 			}
 		}
-		
-		
-		
-		
-		/*if(osa == 0 ){
-			if(uIvici(1) || uIvici(2)){
-				if(c[osa] + za >= space.getsGranica1()[osa] && c[osa] + za <= space.getsGranica2()[osa]){
-					return true;
-				}
-				return false;
-			}else{
-				if(c[osa] + za >= space.getsGranica1()[osa] && c[osa] + za <= space.getuGranica1()[osa] || c[osa] + za >= space.getsGranica2()[osa] && c[osa] + za <= space.getuGranica2()[osa]){
-					return true;
-				}
-				return false;
-			}
-		}else if(osa == 1){
-			if(uIvici(0) || uIvici(2)){
-				if(c[osa] + za >= space.getsGranica1()[osa] && c[osa] + za <= space.getsGranica2()[osa]){
-					return true;
-				}
-				return false;
-			}else{
-				if(c[osa] + za >= space.getsGranica1()[osa] && c[osa] + za <= space.getuGranica1()[osa] || c[osa] + za >= space.getsGranica2()[osa] && c[osa] + za <= space.getuGranica2()[osa]){
-					return true;
-				}
-				return false;
-			}
-		}else if(osa == 2){
-			if(uIvici(0) || uIvici(1)){
-				if(c[osa] + za >= space.getsGranica1()[osa] && c[osa] + za <= space.getsGranica2()[osa]){
-					return true;
-				}
-				return false;
-			}else{
-				if(c[osa] + za >= space.getsGranica1()[osa] && c[osa] + za <= space.getuGranica1()[osa] || c[osa] + za >= space.getsGranica2()[osa] && c[osa] + za <= space.getuGranica2()[osa]){
-					return true;
-				}
-				return false;
-			}
-		}*/
+	
 		return false;
 	}
-	/*private int pomeri(int k, int i){
-
-		poruka = "";
-		if(k + i == sGranica1 || k + i == sGranica2){
-			poruka = "//udario";
-			return k + i;
-		}
-		if(k == sGranica1 && i == -1 || k == sGranica2  && i == 1){
-			return k;
-		}
-		return k + i;
-	}
-
-	private int pomeriSigurno(int k, int i){
-
-		poruka = "";
-		if(k + i == sGranica1 || k + i == uGranica1 || k + i == uGranica2 || k + i == sGranica2){
-			poruka = "//udario";
-			return k + i;
-		}
-		if(k == sGranica1 && i == -1 || k == uGranica1  && i == 1|| k == uGranica2  && i == -1|| k == sGranica2  && i == 1){
-
-			return k;
-		}
-		return k + i;
-	}*/
-
-
 
 	public String moveUp() {
-		if(sme(1,1))
+		
 			c[1]++;
 		return getFormatedCoordinates();
 	}
 
 
 	public String moveDown() {
-		if(sme(1,-1))
+		
 			c[1]--;
 		return getFormatedCoordinates();
 	}
 
 
 	public String moveRight() {
-		if(sme(0,1))
+		
 			c[0]++;
 		return getFormatedCoordinates();
 	}
 
 
 	public String moveLeft() {
-		if(sme(0,-1))
+		
 			c[0]--;
 		return getFormatedCoordinates();
 	}
 
 
 	public String moveBack() {
-		if(sme(2,1))
+		
 			c[2]++;
 		return getFormatedCoordinates();
 	}
 
 
 	public String moveForth() {
-		if(sme(2,-1))
+		
 			c[2]--;
 		return getFormatedCoordinates();
 	}
@@ -161,10 +121,12 @@ public class DroneTacka extends DoubleCubeDrone implements StandardDrone {
 	public String getFormatedCoordinates()
 	{
 
-		return "My position is: (" + Integer.toString(c[0]) + ","
+		return  "(" + Integer.toString(c[0]) + ","
 				+ Integer.toString(c[1]) + ","  
 				+ Integer.toString(c[2]) + ") ";
 	}
 
+
+	
 
 }
--- src/tests/CubeDroneTest.java
+++ src/tests/CubeDroneTest.java
@@ -5,82 +5,218 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import drone.StandardDrone;
+import space.DoubleCubeDrone;
 import space.DroneTacka;
 import space.FlySpace;
 
 public class CubeDroneTest {
 	@Test
-	public void test1(){
+	public void testGranicniUp1(){
+
+		int[] startCoordinates = {39,8,39};
 		
-	
+		DoubleCubeDrone d = new DoubleCubeDrone(startCoordinates);
 		
-		int[] startCoordinates = {30,0,30};
-		DroneTacka d = new DroneTacka(startCoordinates);
+		helperMoveUp(5, d);
 		
-		System.out.println("sgranica1: " + d.space.getsGranica2()[0] + " ugranica 1: " + d.space.getuGranica2()[0]);
-		
-		System.out.println("-------------Moving up-------------");
-		for(int i = 0; i <= 12; i++){
-			System.out.println(d.moveUp());
-		}
-		
-		System.out.println("-------------Moving right-------------");
-		for(int i = 0; i <= 20; i++){
-			System.out.println(d.moveRight());
-		}
-		
-		System.out.println("-------------Moving up-------------");
-		for(int i = 0; i <= 22; i++){
-			System.out.println(d.moveUp());
-		}
-		
-		System.out.println("-------------Moving forth-------------");
-		for(int i = 0; i <= 15; i++){
-			System.out.println(d.moveForth());
-		}
-		
-		System.out.println("-------------Moving left-------------");
-		for(int i = 0; i <= 11; i++){
-			System.out.println(d.moveLeft());
-		}
-		
-		System.out.println("-------------Moving forth-------------");
-		for(int i = 0; i <= 4; i++){
-			System.out.println(d.moveForth());
-		}
-		
-				
-		System.out.println("-------------Moving left-------------");
-		for(int i = 0; i <= 17; i++){
-			System.out.println(d.moveLeft());
-		}
-		
-		System.out.println("-------------Moving up-------------");
-		for(int i = 0; i <= 16; i++){
-			System.out.println(d.moveUp());
-		}
-		
-		System.out.println("-------------Moving back-------------");
-		for(int i = 0; i <= 20; i++){
-			System.out.println(d.moveBack());
-		}
-		
-		
-		System.out.println("-------------Moving left-------------");
-		for(int i = 0; i <= 21; i++){
-			System.out.println(d.moveLeft());
-		}
-		
-		System.out.println("-------------Moving down-------------");
-		for(int i = 0; i <= 19; i++){
-			System.out.println(d.moveDown());
-		}
-		
-		System.out.println(d.granice());
-		
-		assertEquals("0,10,40,50---0,10,40,50---0,10,40,50---", d.granice());
+		assertEquals(d.getFormatedCoordinates(), "(39,9,39) ");
 	}
 	
+	
+	@Test
+	public void testGranicniUp2(){
+
+		int[] startCoordinates = {9,7,35};
+		
+		DoubleCubeDrone d = new DoubleCubeDrone(startCoordinates);
+		
+		helperMoveUp(5, d);
+		
+		assertEquals(d.getFormatedCoordinates(), "(9,8,35) ");
+	}
+	
+	@Test
+	public void testGranicniDown1(){
+
+		int[] startCoordinates = {39,41,39};
+		
+		DoubleCubeDrone d = new DoubleCubeDrone(startCoordinates);
+		
+		helperMoveDown(5, d);
+		
+		assertEquals(d.getFormatedCoordinates(), "(39,40,39) ");
+	}
+	
+	
+	@Test
+	public void testGranicnDown2(){
+
+		int[] startCoordinates = {9,41,35};
+		
+		DoubleCubeDrone d = new DoubleCubeDrone(startCoordinates);
+		
+		helperMoveDown(5, d);
+		
+		assertEquals(d.getFormatedCoordinates(), "(9,39,35) ");
+	}
+	
+	
+	
+	@Test
+	public void testGranicniRight1(){
+
+		int[] startCoordinates = {8,39,39};
+		
+		DoubleCubeDrone d = new DoubleCubeDrone(startCoordinates);
+		
+		helperMoveRight(5, d);
+		
+		assertEquals(d.getFormatedCoordinates(), "(9,39,39) ");
+	}
+	
+	@Test
+	public void testGranicniRight2(){
+
+		int[] startCoordinates = {7,9,39};
+		
+		DoubleCubeDrone d = new DoubleCubeDrone(startCoordinates);
+		
+		helperMoveRight(5, d);
+		
+		assertEquals(d.getFormatedCoordinates(), "(8,9,39) ");
+	}
+	
+	
+	@Test
+	public void testGranicniLeft1(){
+
+		int[] startCoordinates = {41,39,39};
+		
+		DoubleCubeDrone d = new DoubleCubeDrone(startCoordinates);
+		
+		helperMoveLeft(5, d);
+		
+		assertEquals(d.getFormatedCoordinates(), "(40,39,39) ");
+	}
+	
+	@Test
+	public void testGranicniLeft2(){
+
+		int[] startCoordinates = {41,9,39};
+		
+		DoubleCubeDrone d = new DoubleCubeDrone(startCoordinates);
+		
+		helperMoveLeft(5, d);
+		
+		assertEquals(d.getFormatedCoordinates(), "(39,9,39) ");
+	}
+	
+	@Test
+	public void testGranicniForth1(){
+
+		int[] startCoordinates = {39,39,42};
+		
+		DoubleCubeDrone d = new DoubleCubeDrone(startCoordinates);
+		
+		helperMoveForth(5, d);
+		
+		assertEquals(d.getFormatedCoordinates(), "(39,39,41) ");
+	}
+	
+	@Test
+	public void testGranicniForth2(){
+
+		int[] startCoordinates = {9,39,42};
+		
+		DoubleCubeDrone d = new DoubleCubeDrone(startCoordinates);
+		
+		helperMoveForth(5, d);
+		
+		assertEquals(d.getFormatedCoordinates(), "(9,39,37) ");
+	}
+	
+	@Test
+	public void testGranicniBack1(){
+
+		int[] startCoordinates = {39,39,9};
+		
+		DoubleCubeDrone d = new DoubleCubeDrone(startCoordinates);
+		
+		helperMoveBack(5, d);
+		
+		assertEquals(d.getFormatedCoordinates(), "(39,39,10) ");
+	}
+	
+	
+	@Test
+	public void testGranicniBack2(){
+
+		int[] startCoordinates = {9,39,8};
+		
+		DoubleCubeDrone d = new DoubleCubeDrone(startCoordinates);
+		
+		helperMoveBack(5, d);
+		
+		assertEquals(d.getFormatedCoordinates(), "(9,39,13) ");
+	}
+	
+	
+	@Test
+	public void testNullKoordinate(){
+
+		
+		
+		DoubleCubeDrone d = new DoubleCubeDrone();
+		
+		assertEquals(d.getFormatedCoordinates(), "(30,0,30) ");
+	}
+	
+	@Test
+	public void testNullGranice(){
+
+		
+		
+		DoubleCubeDrone d = new DoubleCubeDrone();
+		
+		
+		assertEquals(d.space.getOkvir()[0], 50);
+	}
+	
+	public void helperMoveUp(int n,DoubleCubeDrone drone ){
+		for(int i = 0; i < n; i++){
+			drone.moveUp();
+		}
+	}
+	
+	public void helperMoveDown(int n,DoubleCubeDrone drone){
+		for(int i = 0; i < n; i++){
+			drone.moveDown();
+		}
+	}
+	
+	public void helperMoveRight(int n,DoubleCubeDrone drone){
+		for(int i = 0; i < n; i++){
+			drone.moveRight();
+		}
+	}
+	
+	public void helperMoveLeft(int n,DoubleCubeDrone drone){
+		for(int i = 0; i < n; i++){
+			drone.moveLeft();
+		}
+	}
+	
+	public void helperMoveForth(int n,DoubleCubeDrone drone){
+		for(int i = 0; i < n; i++){
+			drone.moveForth();
+		}
+	}
+	
+	public void helperMoveBack(int n,DoubleCubeDrone drone){
+		for(int i = 0; i < n; i++){
+			drone.moveBack();
+		}
+	}
 	
 	
 	

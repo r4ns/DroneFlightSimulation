dc4c91901014df298798c95572963a84ab5bc603
it54g2015 <it54g2015@ws4.itc03.edu>
2017-03-31 13:06:56 +0200

    Kod sa vezbi

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,10 +1,15 @@
 package drone;
 
+import space.FlySpace;
+
 public class Drone implements StandardDrone{
 	private int x=30;
 	private int y=0;
 	private int z=30;
 
+	FlySpace granice =new FlySpace();
+	
+	
 	public Drone (){
 
 	}
@@ -15,7 +20,9 @@ public class Drone implements StandardDrone{
 	}
 //
 	public String moveUp() {
-		if(y<10 || (x<=10 && y>=10 && y<50 && z<=50) || (x<=50 && y>=10 && y<50 && z<=10) || (x>=40 && x<=50 && y>=10 && y<50 && z<=50) || (x<=50 && y>=10 && y<50 && z>=40 && z<=50) || (x<=50 && y>=40 && y<50 && z<=50 )){
+		if(y<10 || (x<=10 && y>=10 && y<50 && z<=50) || (x<=50 && y>=10 && y<50 && z<=10) || (x>=40 && x<=50 && y>=10 && y<50 && z<=50) || (x<=50 && y>=10 && y<50 && z>=40 && z<=50) || (x<=50 && y>=40 && y<50 && z<=50 ))
+		//if(y<granice.getInnerStart() || (x<=granice.getInnerStart() && y>=granice.getInnerStart() && y<granice.getOuterFinish() && z<=granice.getOuterFinish()) || (x<=granice.getOuterFinish() && y>=granice.getInnerStart() && y<granice.getOuterFinish() && z<=granice.getInnerStart()) || (x>=granice.getInnerFinish() && x<=granice.getOuterFinish() && y>=granice.getInnerStart() && y<granice.getOuterFinish() && z<=granice.getOuterFinish()) || (x<=granice.getOuterFinish() && y>=granice.getInnerStart() && y<granice.getOuterFinish() && z>=granice.getInnerFinish() && z<=granice.getOuterFinish()) || (x<=granice.getOuterFinish() && y>=granice.getInnerFinish() && y<granice.getOuterFinish() && z<=granice.getOuterFinish()))
+		{
 			y=y+1;
 			return getFormatedCoordinates();
 		}
@@ -25,7 +32,8 @@ public class Drone implements StandardDrone{
 	}
 
 	public String moveDown() {
-		if((x<=10 && y>0 && y<=50 && z<=50) || (x<=50 && y>0 && y<=50 && z<=10) || (x>=40 && x<=50 && y>0 && y<=50 && z<=50) || (x<=50 && y>0 && y<50 && z>=40 && z<=50) || (x<=50 && y>40 && y<=50 && z<=50) || (x<=50 && y>0 && y<=10 &&z<=50)){
+		//if((x<=10 && y>0 && y<=granice.getOuterFinish() && z<=granice.getOuterFinish()) || (x<=granice.getOuterFinish() && y>0 && y<=granice.getOuterFinish() && z<=10) || (x>=40 && x<=granice.getOuterFinish() && y>0 && y<=granice.getOuterFinish() && z<=granice.getOuterFinish()) || (x<=granice.getOuterFinish() && y>0 && y<granice.getOuterFinish() && z>=40 && z<=granice.getOuterFinish()) || (x<=granice.getOuterFinish() && y>40 && y<=granice.getOuterFinish() && z<=granice.getOuterFinish()) || (x<=granice.getOuterFinish() && y>0 && y<=10 &&z<=granice.getOuterFinish())){
+		if(y>1){
 			y=y-1;
 			return getFormatedCoordinates();
 		}
@@ -35,7 +43,8 @@ public class Drone implements StandardDrone{
 	}
 
 	public String moveLeft() {
-		if((x>0 && y>0 &&y<=10 && z<=50) || (x>0 && y>=40 && y<=50  && z<=50) || (x>0 &&x<=10 && y<=50 && z<=50) || (x>0 && x<=50 && y<=50 && z<=10) || (x>0 && x<=50 && y<=50 && z>=40) || (x>40 && x<=50 && y>=10 && y<=40 && z>=10 && z<=40)){
+		//if((x>0 && y>0 &&y<=10 && z<=granice.getOuterFinish()) || (x>0 && y>=40 && y<=granice.getOuterFinish()  && z<=granice.getOuterFinish()) || (x>0 &&x<=10 && y<=granice.getOuterFinish() && z<=granice.getOuterFinish()) || (x>0 && x<=granice.getOuterFinish() && y<=granice.getOuterFinish() && z<=10) || (x>0 && x<=granice.getOuterFinish() && y<=granice.getOuterFinish() && z>=40) || (x>40 && x<=granice.getOuterFinish() && y>=10 && y<=40 && z>=10 && z<=40))
+		if(x>0){
 			x=x-1;
 			return getFormatedCoordinates();
 		}
---
+++ src/space/FlySpace.java
@@ -0,0 +1,74 @@
+package space;
+
+public class FlySpace {
+	
+	public int outerStart=0;
+	public int outerFinish=50;
+	public int innerStart=10;
+	public int innerFinish=40;
+	
+	
+	public FlySpace() {
+		
+	}
+	
+	public FlySpace(int oBS, int oBF, int iBS, int iBF){
+		outerStart=oBS;
+		outerFinish=oBF;
+		innerStart=iBS;
+		innerFinish=iBF;
+	}
+
+	public int getOuterStart() {
+		return outerStart;
+	}
+
+	public void setOuterStart(int outerStart) {
+		this.outerStart = outerStart;
+	}
+
+	public int getOuterFinish() {
+		return outerFinish;
+	}
+
+	public void setOuterFinish(int outerFinish) {
+		this.outerFinish = outerFinish;
+	}
+
+	public int getInnerStart() {
+		return innerStart;
+	}
+
+	public void setInnerStart(int innerStart) {
+		this.innerStart = innerStart;
+	}
+
+	public int getInnerFinish() {
+		return innerFinish;
+	}
+
+	public void setInnerFinish(int innerFinish) {
+		this.innerFinish = innerFinish;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	
+	
+}

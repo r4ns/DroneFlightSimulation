d1b9f3562b1fd4e5faf75afb963e2500b04045a9
Igor <Igor@192.168.0.10>
2017-04-28 00:59:01 +0200

    IT42-2015 Igor Sember

---
+++ src/space/CubeTest.java
@@ -0,0 +1,45 @@
+package space;
+
+public class CubeTest {
+	Cube cube;
+	
+	@Test
+	public void testIfCoordinatesAreInCube(){
+		
+	}
+	
+	@Test
+	public void testIfCubeIsInSomePartContainedInOtherCube(){
+		
+	}
+	
+	@Test
+	public void testIfCubesAreTouchingFromInsideInOnePoint(){
+		
+	}
+	
+	@Test
+	public void testIfCubesAreTouchingFromInsideAlongOneCubeSide(){
+		
+	}
+	
+	@Test
+	public void testIfCubesAreTouchingFromOutsideInOnePoint(){
+		
+	}
+	
+	@Test
+	public void testIfCubesAreTouchingFromOutsideAlongOneCubeSide(){
+		
+	}
+	
+	@Test
+	public void testIfCubesAreTouchingFromOutside(){
+		
+	}
+	
+	@Test
+	public void testIfCubesAreTouchingFromInside(){
+		
+	}
+}

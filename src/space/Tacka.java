1a74abd418715ca8fcde667d69eff9041abe5107
lukar <lukar@DESKTOP-E09RVRJ>
2017-04-27 12:50:18 +0200

    Napravljen paket space u kom se nalaze implementacije potrebne za DoubleCubedDrone

---
+++ src/space/Tacka.java
@@ -0,0 +1,53 @@
+package space;
+
+public class Tacka {
+	 
+    private int x;
+    private int y;
+    private int z;
+   
+    public Tacka(int x, int y, int z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+   
+   
+    public Tacka() {
+        // TODO Auto-generated constructor stub
+    }
+ 
+ 
+    public int getX() {
+        return x;
+    }
+ 
+ 
+    public int getY() {
+        return y;
+    }
+ 
+ 
+    public int getZ() {
+        return z;
+    }
+ 
+ 
+    public void setX(int x) {
+        this.x = x;
+    }
+ 
+ 
+    public void setY(int y) {
+        this.y = y;
+    }
+ 
+ 
+    public void setZ(int z) {
+        this.z = z;
+    }
+ 
+ 
+   
+   
+}

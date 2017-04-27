7c17a97316f28792f8864c556fd85882b4e1784c
StefanKeler <stefankeler96@gmail.com>
2017-04-27 11:49:31 +0200

    testovi

--- src/util/Cube.java
+++ src/util/Cube.java
@@ -8,7 +8,7 @@ public class Cube {
 	public Cube(int [] cubeStartCoordinates, int side)
 	{
 		minCoordinates = cubeStartCoordinates;
-		maxCoordinates = new int[]{minCoordinates[0] + side, minCoordinates[1] + side, minCoordinates[2] - side};
+		maxCoordinates = new int[]{minCoordinates[0] + side, minCoordinates[1] + side, minCoordinates[2] + side};
 		cubeSideLength = side;
 	}
 	//kopira prosledjenu kocku
@@ -34,21 +34,47 @@ public class Cube {
 	//proverava da li se kocke seku
 	public boolean checkCubeIntersection(Cube cube)
 	{
-
-		if(checkCoordinates(cube.maxCoordinates) || checkCoordinates(cube.minCoordinates))
-			return true;
-
+		int [][] minCoordinates1 = 
+			{{minCoordinates[0], minCoordinates[1], minCoordinates[2]}, 
+			{minCoordinates[0], minCoordinates[1] + 1, minCoordinates[2]}, 
+			{minCoordinates[0] + 1, minCoordinates[1] + 1, minCoordinates[2]}, 
+			{minCoordinates[0] + 1, minCoordinates[1], minCoordinates[2]}};
+		
+		int [][] maxCoordinates1 = 
+			{{maxCoordinates[0], maxCoordinates[1], maxCoordinates[2]}, 
+			{maxCoordinates[0], maxCoordinates[1] - 1, maxCoordinates[2]}, 
+			{maxCoordinates[0] - 1, maxCoordinates[1] - 1, maxCoordinates[2]}, 
+			{maxCoordinates[0] - 1, maxCoordinates[1], maxCoordinates[2]}};
+		
+		for(int i = 0; i < minCoordinates1.length; i++){
+			if(cube.checkCoordinates(maxCoordinates1[i]) || cube.checkCoordinates(minCoordinates1[i]))
+				return true;
+		}
+		
 		return false;
 	}
 	//proverava da li se kocke dodiruju
 	public boolean checkIfCubesAreTouching(Cube cube)
 	{
+		if(!checkCubeIntersection(cube))
+			return false;
 		for(int i = 0; i < 3; i++){
-			if(cube.maxCoordinates[i] == this.maxCoordinates[i] || cube.minCoordinates[i] == this.maxCoordinates[i])
+			if(
+					maxCoordinates[i%3] >= cube.minCoordinates[i%3] && maxCoordinates[i%3] < cube.maxCoordinates[i%3] &&
+					maxCoordinates[(i+1)%3] >= cube.minCoordinates[(i+1)%3] && maxCoordinates[(i+1)%3] <= cube.maxCoordinates[(i+1)%3] &&	
+					(maxCoordinates[(i+2)%3] == cube.minCoordinates[(i+2)%3] || maxCoordinates[(i+2)%3] == cube.maxCoordinates[(i+2)%3]) || 
+					minCoordinates[i%3] >= cube.minCoordinates[i%3] && minCoordinates[i%3] <= cube.maxCoordinates[i%3] &&
+					minCoordinates[(i+1)%3] >= cube.minCoordinates[(i+1)%3] && minCoordinates[(i+1)%3] <= cube.maxCoordinates[(i+1)%3] &&	
+					(minCoordinates[(i+2)%3] == cube.minCoordinates[(i+2)%3] || minCoordinates[(i+2)%3] == cube.maxCoordinates[(i+2)%3])
+			
+			){
 				return true;
+			}
 		}
 		return false;
 	}
+	
+	
 	@Override
 	public String toString()
 	{
@@ -64,13 +90,50 @@ public class Cube {
 	//proverava da li se kocke dodiruju iznutra
 	public boolean checkIfCubesAreTouchingFromInside(Cube cube)
 	{
-		//implementacija treba da koristi checkIfCubesAreTouching
+		if(!checkIfCubesAreTouching(cube))
+			return false;
+		
+		if(!checkCubeIntersection(cube))
+			return false;
+		for(int i = 0; i < 3; i++){
+			if(
+					maxCoordinates[i%3] >= cube.minCoordinates[i%3] && maxCoordinates[i%3] < cube.maxCoordinates[i%3] &&
+					maxCoordinates[(i+1)%3] >= cube.minCoordinates[(i+1)%3] && maxCoordinates[(i+1)%3] <= cube.maxCoordinates[(i+1)%3] &&	
+					(maxCoordinates[(i+2)%3] >= cube.minCoordinates[(i+2)%3] || maxCoordinates[(i+2)%3] >= cube.maxCoordinates[(i+2)%3]) && 
+					minCoordinates[i%3] >= cube.minCoordinates[i%3] && minCoordinates[i%3] <= cube.maxCoordinates[i%3] &&
+					minCoordinates[(i+1)%3] >= cube.minCoordinates[(i+1)%3] && minCoordinates[(i+1)%3] <= cube.maxCoordinates[(i+1)%3] &&	
+					(minCoordinates[(i+2)%3] >= cube.minCoordinates[(i+2)%3] || minCoordinates[(i+2)%3] >= cube.maxCoordinates[(i+2)%3])
+			
+			){
+				return true;
+			}
+		}
+		
 		return false;
 	}
 	//proverava da li se kocke dodiruju sa spoljasnje strane
 	public boolean checkIfCubesAreTouchingFromOutside(Cube cube)
 	{
 		//implementacija treba da koristi checkIfCubesAreTouching
+		if(!checkIfCubesAreTouching(cube))
+			return false;
+		
+		if(!checkCubeIntersection(cube))
+			return false;
+		for(int i = 0; i < 3; i++){
+			if(
+					maxCoordinates[i%3] >= cube.minCoordinates[i%3] && maxCoordinates[i%3] < cube.maxCoordinates[i%3] &&
+					maxCoordinates[(i+1)%3] >= cube.minCoordinates[(i+1)%3] && maxCoordinates[(i+1)%3] <= cube.maxCoordinates[(i+1)%3] &&	
+					(maxCoordinates[(i+2)%3] == cube.minCoordinates[(i+2)%3] || maxCoordinates[(i+2)%3] == cube.maxCoordinates[(i+2)%3]) || 
+					minCoordinates[i%3] >= cube.minCoordinates[i%3] && minCoordinates[i%3] <= cube.maxCoordinates[i%3] &&
+					minCoordinates[(i+1)%3] >= cube.minCoordinates[(i+1)%3] && minCoordinates[(i+1)%3] <= cube.maxCoordinates[(i+1)%3] &&	
+					(minCoordinates[(i+2)%3] == cube.minCoordinates[(i+2)%3] || minCoordinates[(i+2)%3] == cube.maxCoordinates[(i+2)%3])
+			
+			){
+				return true;
+			}
+		}
+		
 		return false;
 	}
 	//uvecava X
@@ -111,7 +174,7 @@ public class Cube {
 	}
 	public int[] getMinCoordinates() {
 		return minCoordinates;
-	}
+	} 	
 	public void setMinCoordinates(int[] minCoordinates) {
 		this.minCoordinates = minCoordinates;
 	}
